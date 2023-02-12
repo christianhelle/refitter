e69f6a4c63420766b578938cb8df7aa80bbbb014
Christian Helle <christian.helle@outlook.com>
2023-02-13 00:46:46 +0100

    Add convenience properties in EmbeddedResourece for reading Swagger.json V2 and V3

---
+++ src/Refitter.Tests/Resources/EmbeddedResource.cs
@@ -0,0 +1,29 @@
+﻿namespace Refitter.Tests.Resources;
+
+public static class EmbeddedResources
+{
+    private static readonly Type Type = typeof(EmbeddedResources);
+
+    private static Stream GetStream(string name)
+        => Type.Assembly.GetManifestResourceStream(Type, name)!;
+
+    public static string SwaggerPetstoreJsonV2
+    {
+        get
+        {
+            using var stream = GetStream("V2.Swagger.json");
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+
+    public static string SwaggerPetstoreJsonV3
+    {
+        get
+        {
+            using var stream = GetStream("V3.Swagger.json");
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+}
