926256967bcf1fb1fe11b8b90fcb43a275f014f4
Christian Helle <christian.helle@outlook.com>
2023-02-13 00:47:18 +0100

    Introduce RefitGenerator

---
+++ src/Refitter.Core/RefitGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NJsonSchema.CodeGeneration.CSharp;
+using NSwag;
+using NSwag.CodeGeneration.CSharp;
+
+namespace Refitter.Core
+{
+    public class RefitGenerator
+    {
+        public async Task<string> Generate(string swaggerFile)
+        {
+            var document = await (swaggerFile.EndsWith("yaml") || swaggerFile.EndsWith("yml")
+                ? OpenApiYamlDocument.FromFileAsync(swaggerFile)
+                : OpenApiDocument.FromFileAsync(swaggerFile));
+
+            var contracts = GenerateContracts(document);
+            var client = GenerateClient(document);
+            return contracts;
+        }
+
+        private static string GenerateClient(OpenApiDocument document)
+        {
+            var code = new StringBuilder();
+            code.AppendLine("public interface IApiClient")
+                .AppendLine("{");
+
+            foreach (var kv in document.Paths)
+            {
+                var path = kv.Key;
+                var item = kv.Value;
+            }
+
+            code.AppendLine("}")
+                .AppendLine();
+            
+            return code.ToString();
+        }
+
+        private static string GenerateContracts(OpenApiDocument document)
+        {
+            var settings = new CSharpClientGeneratorSettings
+            {
+                GenerateClientClasses = false,
+                GenerateDtoTypes = true,
+                GenerateClientInterfaces = false,
+            };
+
+            settings.CSharpGeneratorSettings.JsonLibrary = CSharpJsonLibrary.SystemTextJson;
+            var generator = new CSharpClientGenerator(document, settings);
+            return generator.GenerateFile();
+        }
+    }
+}
---
+++ src/Refitter.Tests/RefitGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Refitter.Core;
+using Refitter.Tests.Resources;
+using Xunit;
+
+namespace Refitter.Tests;
+
+public class RefitGeneratorTests
+{
+    [Fact]
+    public async Task Generate()
+    {
+        var contents = EmbeddedResources.SwaggerPetstoreJsonV3;
+        var folder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(folder);
+        var swaggerFile = Path.Combine(folder, "Swagger.json");
+        await File.WriteAllTextAsync(swaggerFile, contents);
+
+        var generator = new RefitGenerator();
+        var result = await generator.Generate(swaggerFile);
+        result.Should().NotBeNullOrWhiteSpace();
+    }
+}
