e67480be0c10f1211c82392f1619242959177e52
Christian Helle <christian.helle@outlook.com>
2023-02-17 00:29:10 +0100

    Implement simple CLI tool that accepts the path to an OpenAPI spec file and returns the generated code

--- src/Refitter/Program.cs
+++ src/Refitter/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.ComponentModel;
+using Refitter.Core;
+using Spectre.Console;
+using Spectre.Console.Cli;
 
-Console.WriteLine("Hello, World!");
+
+var app = new CommandApp<GenerateCommand>();
+return app.Run(args);
+
+internal sealed class GenerateCommand : AsyncCommand<GenerateCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [Description("Path to OpenAPI Specification file")]
+        [CommandArgument(0, "[openApiPath]")]
+        public string? OpenApiPath { get; init; }
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    {
+        var searchPath = settings.OpenApiPath ?? ".";
+        
+        var generator= new RefitGenerator();
+        var code = await generator.Generate(searchPath);
+        
+        AnsiConsole.WriteLine(code);
+        return 0;
+    }
+}
