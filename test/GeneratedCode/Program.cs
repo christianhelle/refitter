6669652c531989292383337604a7a5ea1f1c49a5
Christian Helle <christian.helle@outlook.com>
2023-02-17 22:36:25 +0100

    Add Program.cs to .NET Framework and Core generated code projects

---
+++ test/GeneratedCode/Program.cs
@@ -0,0 +1,20 @@
+using Refit;
+using System;
+using MyNamespace;
+using System.Threading.Tasks;
+
+namespace MyNamespace;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var client = RestService.For<ISwaggerPetstore>("https://petstore3.swagger.io/api/v3");
+        var pet = await client.GetPetById(2);
+
+        Console.WriteLine($"Name: {pet.Name}");
+        Console.WriteLine($"Category: {pet.Category.Name}");
+        Console.WriteLine($"Status: {pet.Status}");
+        Console.ReadLine();
+    }
+}
