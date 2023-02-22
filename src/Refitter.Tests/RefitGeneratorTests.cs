7e4ec40b7a9460970df9b515f05fd6d8c470f28c
Christian Helle <christian.helle@outlook.com>
2023-02-22 22:54:48 +0100

    Update RefitGenerator unit tests to only run build tests in Release builds

--- src/Refitter.Tests/RefitGeneratorTests.cs
+++ src/Refitter.Tests/RefitGeneratorTests.cs
@@ -8,32 +8,36 @@ namespace Refitter.Tests;
 public class RefitGeneratorTests
 {
     [Theory]
-    [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV2, "SwaggerPetstore.json")]
     [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV3, "SwaggerPetstore.json")]
-    [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV2, "SwaggerPetstore.yaml")]
     [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV3, "SwaggerPetstore.yaml")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV2, "SwaggerPetstore.json")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV2, "SwaggerPetstore.yaml")]
     public async Task Can_Generate_Code(SampleOpenSpecifications version, string filename)
     {
-        var swaggerFile = await CreateSwaggerFile(EmbeddedResources.GetSwaggerPetstore(version), filename);
-        var sut = new RefitGenerator();
-        var result = await sut.Generate(swaggerFile, "GeneratedCode");
-        result.Should().NotBeNullOrWhiteSpace();
+        var generateCode = await GenerateCode(version, filename);
+        generateCode.Should().NotBeNullOrWhiteSpace();
     }
 
+#if !DEBUG
     [Theory]
-    [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV2, "SwaggerPetstore.json")]
     [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV3, "SwaggerPetstore.json")]
-    [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV2, "SwaggerPetstore.yaml")]
     [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV3, "SwaggerPetstore.yaml")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV2, "SwaggerPetstore.json")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV2, "SwaggerPetstore.yaml")]
+#endif
     public async Task Can_Build_Generated_Code(SampleOpenSpecifications version, string filename)
     {
-        var swaggerFile = await CreateSwaggerFile(EmbeddedResources.GetSwaggerPetstore(version), filename);
-        var sut = new RefitGenerator();
-        var result = await sut.Generate(swaggerFile, "GeneratedCode");
+        var generateCode = await GenerateCode(version, filename);
         BuildHelper
-            .BuildCSharp(result)
+            .BuildCSharp(generateCode)
             .Should()
             .BeTrue();
+    }
+
+    private static async Task<string> GenerateCode(SampleOpenSpecifications version, string filename)
+    {
+        var swaggerFile = await CreateSwaggerFile(EmbeddedResources.GetSwaggerPetstore(version), filename);
+        return await new RefitGenerator().Generate(swaggerFile, "GeneratedCode");
     }
 
     private static async Task<string> CreateSwaggerFile(string contents, string filename)
