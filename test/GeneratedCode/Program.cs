e7b2cddb86cdafd5771aa2c3cf0959e8724e0518
Christian Helle <christian.helle@outlook.com>
2023-02-22 22:26:22 +0100

    Comment out test code in smoke test Program.cs

--- test/GeneratedCode/Program.cs
+++ test/GeneratedCode/Program.cs
@@ -9,12 +9,12 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        var client = RestService.For<ISwaggerPetstore>("https://petstore3.swagger.io/api/v3");
-        var pet = await client.GetPetById(2);
+        // var client = RestService.For<ISwaggerPetstore>("https://petstore3.swagger.io/api/v3");
+        // var pet = await client.GetPetById(2);
 
-        Console.WriteLine($"Name: {pet.Name}");
-        Console.WriteLine($"Category: {pet.Category.Name}");
-        Console.WriteLine($"Status: {pet.Status}");
-        Console.ReadLine();
+        // Console.WriteLine($"Name: {pet.Name}");
+        // Console.WriteLine($"Category: {pet.Category.Name}");
+        // Console.WriteLine($"Status: {pet.Status}");
+        // Console.ReadLine();
     }
 }
