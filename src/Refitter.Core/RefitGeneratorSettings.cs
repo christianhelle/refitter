65be2e9fb2c488c6f77367afddea6c0102af34fc
Christian Helle <christian.helle@outlook.com>
2023-02-24 23:21:13 +0100

    Introduce RefitGeneratorSettings

---
+++ src/Refitter.Core/RefitGeneratorSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Refitter.Core
+{
+    [ExcludeFromCodeCoverage]
+    public class RefitGeneratorSettings
+    {
+        public string OpenApiPath { get; set; }
+
+        public string Namespace { get; set; } = "GeneratedCode";
+
+        public NamingSettings Naming { get; set; } = new();
+
+        public bool GenerateContracts { get; set; } = true;
+
+        public bool GenerateCodeComments { get; set; }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public class NamingSettings
+    {
+        public bool UseOpenApiTitle { get; set; } = true;
+
+        public string ClassName { get; set; } = "ApiClient";
+    }
+}
