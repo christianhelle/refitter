263ab7fe8c1ff5283803619f4192da38e307c2e2
Christian Helle <christian.helle@outlook.com>
2023-02-27 00:17:08 +0100

    Re-enable Petstore usage in ConsoleApp Program.cs

--- test/ConsoleApp/Program.cs
+++ test/ConsoleApp/Program.cs
@@ -6,18 +6,14 @@ namespace MyNamespace;
 
 internal class Program
 {
-    // private static async Task Main(string[] args)
-    // {
-    //     var client = RestService.For<GeneratedCode.ISwaggerPetstore>("https://petstore3.swagger.io/api/v3");
-    //     var pet = await client.GetPetById(2);
-
-    //     Console.WriteLine($"Name: {pet.Name}");
-    //     Console.WriteLine($"Category: {pet.Category.Name}");
-    //     Console.WriteLine($"Status: {pet.Status}");
-    //     Console.ReadLine();
-    // }
-
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
+        var client = RestService.For<Petstore.ISwaggerPetstore>("https://petstore3.swagger.io/api/v3");
+        var pet = await client.GetPetById(2);
+
+        Console.WriteLine($"Name: {pet.Name}");
+        Console.WriteLine($"Category: {pet.Category.Name}");
+        Console.WriteLine($"Status: {pet.Status}");
+        Console.ReadLine();
     }
 }
