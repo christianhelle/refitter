c9ec845a91e1a6e7ccd5fa7871de5d4f11ee508e
Christian Helle <christian.helle@outlook.com>
2023-03-16 10:19:44 +0100

    Update smoke test Program.cs

--- test/ConsoleApp/Program.cs
+++ test/ConsoleApp/Program.cs
@@ -6,23 +6,19 @@ namespace MyNamespace;
 
 internal class Program
 {
-    // private static async Task Main(string[] args)
-    // {
-    //     var client = RestService.For<Petstore.ISwaggerPetstore>("https://petstore3.swagger.io/api/v3");
-    //     var pet = await client.GetPetById(2);
-
-    //     Console.WriteLine($"Name: {pet.Name}");
-    //     Console.WriteLine($"Category: {pet.Category.Name}");
-    //     Console.WriteLine($"Status: {pet.Status}");
-
-    //     var pets = await client.FindPetsByStatus(Petstore.Status.Available);
-    //     Console.WriteLine("Found " + pets.Count + " available pet(s)");        
-
-    //     var taggedPets = await client.FindPetsByTags(new[] {"tag1Updated", "new"});
-    //     Console.WriteLine("Found " + taggedPets.Count + " tagged pet(s)");
-    // }
-
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
+        var client = RestService.For<Petstore.ISwaggerPetstore>("https://petstore3.swagger.io/api/v3");
+        var pet = await client.GetPetById(2);
+
+        Console.WriteLine($"Name: {pet.Name}");
+        Console.WriteLine($"Category: {pet.Category.Name}");
+        Console.WriteLine($"Status: {pet.Status}");
+
+        var pets = await client.FindPetsByStatus(Petstore.Status.Available);
+        Console.WriteLine("Found " + pets.Count + " available pet(s)");        
+
+        var taggedPets = await client.FindPetsByTags(new[] {"tag1Updated", "new"});
+        Console.WriteLine("Found " + taggedPets.Count + " tagged pet(s)");
     }
 }
