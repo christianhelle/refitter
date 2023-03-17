e994b504ae94b2ed5f82f60116c062ebccc0b796
Christian Helle <christian.helle@outlook.com>
2023-03-17 10:08:22 +0100

    Comment out Petstore usage in smoke test ConsoleApp as the smoke test Github workflow only builds one OpenAPI spec at a time

--- test/ConsoleApp/Program.cs
+++ test/ConsoleApp/Program.cs
@@ -8,12 +8,12 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        var client = RestService.For<Petstore.ISwaggerPetstore>("https://petstore3.swagger.io/api/v3");
-        var pet = await client.GetPetById(2);
+        // var client = RestService.For<Petstore.ISwaggerPetstore>("https://petstore3.swagger.io/api/v3");
+        // var pet = await client.GetPetById(2);
 
-        Console.WriteLine($"Name: {pet.Name}");
-        Console.WriteLine($"Category: {pet.Category.Name}");
-        Console.WriteLine($"Status: {pet.Status}");
+        // Console.WriteLine($"Name: {pet.Name}");
+        // Console.WriteLine($"Category: {pet.Category.Name}");
+        // Console.WriteLine($"Status: {pet.Status}");
 
         // var pets = await client.FindPetsByStatus(Petstore.Status.Available);
         // Console.WriteLine("Found " + pets.Count + " available pet(s)");        
