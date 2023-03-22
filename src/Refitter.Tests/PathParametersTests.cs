b2472621be9a46aba91a68c92f7f9b998f96b15b
Christian Helle <christian.helle@outlook.com>
2023-03-22 15:55:07 +0100

    Add PathParametersTests

---
+++ src/Refitter.Tests/PathParametersTests.cs
@@ -0,0 +1,101 @@
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Refitter.Core;
+using Refitter.Tests.Build;
+using Xunit;
+
+namespace Refitter.Tests;
+
+public class PathParametersTests
+{
+    private const string OpenApiSpec = @"
+swagger: '2.0'
+info:
+  title: Reference parameters
+  version: v0.0.1
+paths:
+  '/orders/{orderId}/order-items/{orderItemId}':
+    parameters:
+      - $ref: '#/parameters/OrderId'
+      - $ref: '#/parameters/OrderItemId'
+    delete:
+      summary: Delete an order item
+      description: >-
+        This method allows to remove an order item from an order, by specifying
+        their ids.
+      responses:
+        '204':
+          description: No Content.
+        default:
+          description: Default response
+          schema:
+            $ref: '#/definitions/Error'
+definitions:
+  Error:
+    type: object
+    properties:
+      message:
+          type: string
+parameters:
+  OrderId:
+    name: orderId
+    in: path
+    description: Identifier of the order.
+    required: true
+    type: string
+    format: uuid
+  OrderItemId:
+    name: orderItemId
+    in: path
+    description: Identifier of the order item.
+    required: true
+    type: string
+    format: uuid
+";
+
+    [Fact]
+    public async Task Can_Generate_Code()
+    {
+        var generateCode = await GenerateCode();
+        generateCode.Should().NotBeNullOrWhiteSpace();
+    }
+
+    [Fact]
+    public async Task Generates_Path_Parameters()
+    {
+        var generateCode = await GenerateCode();
+        using var scope = new AssertionScope();
+        generateCode.Should().Contain("Guid? orderId");
+        generateCode.Should().Contain("Guid? orderItemId");
+    }
+
+    [Fact]
+    public async Task Can_Build_Generated_Code()
+    {
+        var generateCode = await GenerateCode();
+        BuildHelper
+            .BuildCSharp(generateCode)
+            .Should()
+            .BeTrue();
+    }
+
+    private static async Task<string> GenerateCode()
+    {
+        var swaggerFile = await CreateSwaggerFile(OpenApiSpec);
+        var settings = new RefitGeneratorSettings { OpenApiPath = swaggerFile };
+
+        var sut = await RefitGenerator.CreateAsync(settings);
+        var generateCode = sut.Generate();
+        return generateCode;
+    }
+
+    private static async Task<string> CreateSwaggerFile(string contents)
+    {
+        var filename = Guid.NewGuid().ToString() + ".yml";
+        var folder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(folder);
+        var swaggerFile = Path.Combine(folder, filename);
+        await File.WriteAllTextAsync(swaggerFile, contents);
+        return swaggerFile;
+    }
+}
