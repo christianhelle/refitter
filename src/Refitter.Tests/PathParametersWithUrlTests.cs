dc860cd4db466955a33b6bd7855694bd970430e3
Christian Helle <christian.helle@outlook.com>
2023-04-02 22:17:36 +0200

    Remove unused code from PathParametersWithUrlTests

--- src/Refitter.Tests/PathParametersWithUrlTests.cs
+++ src/Refitter.Tests/PathParametersWithUrlTests.cs
@@ -1,5 +1,4 @@
 using FluentAssertions;
-using FluentAssertions.Execution;
 using Refitter.Core;
 using Refitter.Tests.Build;
 using Xunit;
@@ -8,51 +7,6 @@ namespace Refitter.Tests;
 
 public class PathParametersWithUrlTests
 {
-    private const string OpenApiSpec = @"
-swagger: '2.0'
-info:
-  title: Reference parameters
-  version: v0.0.1
-paths:
-  '/orders/{orderId}/order-items/{orderItemId}':
-    parameters:
-      - $ref: '#/parameters/OrderId'
-      - $ref: '#/parameters/OrderItemId'
-    delete:
-      summary: Delete an order item
-      description: >-
-        This method allows to remove an order item from an order, by specifying
-        their ids.
-      responses:
-        '204':
-          description: No Content.
-        default:
-          description: Default response
-          schema:
-            $ref: '#/definitions/Error'
-definitions:
-  Error:
-    type: object
-    properties:
-      message:
-          type: string
-parameters:
-  OrderId:
-    name: orderId
-    in: path
-    description: Identifier of the order.
-    required: true
-    type: string
-    format: uuid
-  OrderItemId:
-    name: orderItemId
-    in: path
-    description: Identifier of the order item.
-    required: true
-    type: string
-    format: uuid
-";
-
     [Fact]
     public async Task Can_Generate_Code()
     {
