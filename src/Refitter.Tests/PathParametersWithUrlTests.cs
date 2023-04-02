165a0b923d04f8886b7900eb75b09e064a9dc6bd
Christian Helle <christian.helle@outlook.com>
2023-04-02 22:18:29 +0200

    Add tests for Petstore v2 and v3 for both JSON and YAML format in PathParametersWithUrlTests

--- src/Refitter.Tests/PathParametersWithUrlTests.cs
+++ src/Refitter.Tests/PathParametersWithUrlTests.cs
@@ -9,6 +9,11 @@ public class PathParametersWithUrlTests
 {
     [Theory]
     [InlineData("https://petstore3.swagger.io/api/v3/openapi.json")]
+#if !DEBUG
+    [InlineData("https://petstore3.swagger.io/api/v3/openapi.yaml")]
+    [InlineData("https://petstore.swagger.io/v2/swagger.json")]
+    [InlineData("https://petstore.swagger.io/v2/swagger.yaml")]
+#endif
     public async Task Can_Generate_Code(string url)
     {
         var generateCode = await GenerateCode(url);
@@ -17,6 +22,11 @@ public class PathParametersWithUrlTests
 
     [Theory]
     [InlineData("https://petstore3.swagger.io/api/v3/openapi.json")]
+#if !DEBUG
+    [InlineData("https://petstore3.swagger.io/api/v3/openapi.yaml")]
+    [InlineData("https://petstore.swagger.io/v2/swagger.json")]
+    [InlineData("https://petstore.swagger.io/v2/swagger.yaml")]
+#endif
     public async Task Can_Build_Generated_Code(string url)
     {
         var generateCode = await GenerateCode(url);
