4b814149f7a93e8f37bcb6c5eef0a2e5d9a66568
Christian Helle <christian.helle@outlook.com>
2023-05-08 22:32:33 +0200

    Introduce SupportInformation for providing anonymous user identity

---
+++ src/Refitter/SupportInformation.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Refitter;
+
+public static class SupportInformation
+{
+    public static string GetSupportKey()
+        => GetAnonymousIdentity().Substring(0, 7);
+
+    public static string GetAnonymousIdentity()
+        => $"{Environment.UserName}@{GetMachineName()}".ToSha256();
+
+    [ExcludeFromCodeCoverage]
+    private static string GetMachineName()
+    {
+        try
+        {
+            return Environment.MachineName;
+        }
+        catch
+        {
+            return "localhost";
+        }
+    }
+
+    private static string ToSha256(this string value)
+    {
+        using var sha256 = SHA256.Create();
+        var bytes = Encoding.UTF8.GetBytes(value);
+        var hash = sha256.ComputeHash(bytes);
+        return Convert.ToBase64String(hash).ToLowerInvariant();
+    }
+}
