cd51d317255ec78c1ad78e0acd4b6a8ac0a5887a
Christian Helle <christian.helle@outlook.com>
2023-06-15 14:42:53 +0200

    Introduce --use-iso-date-format CLI tool argument

--- src/Refitter/Settings.cs
+++ src/Refitter/Settings.cs
@@ -53,4 +53,9 @@ public sealed class Settings : CommandSettings
     [CommandOption("--no-logging")]
     [DefaultValue(false)]
     public bool NoLogging { get; set; }
+
+    [Description("Explicitly format dates query strings parameters with ISO 8601 standard date format using delimiters (2023-06-15)")]
+    [CommandOption("--use-iso-date-format")]
+    [DefaultValue(false)]
+    public bool UseIsoDateFormat { get; set; }
 }
