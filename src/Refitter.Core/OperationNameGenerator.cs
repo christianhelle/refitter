47d08ff38fb45c8ed9a28e5207f13878f2c56c3e
Christian Helle <christian.helle@outlook.com>
2023-06-20 17:45:24 +0200

    Ignore default behavior in OperationNameGenerator in code coverage

--- src/Refitter.Core/OperationNameGenerator.cs
+++ src/Refitter.Core/OperationNameGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using NSwag;
 using NSwag.CodeGeneration.OperationNameGenerators;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace Refitter.Core;
@@ -16,8 +17,10 @@ public class OperationNameGenerator : IOperationNameGenerator
             defaultGenerator = new MultipleClientsFromFirstTagAndPathSegmentsOperationNameGenerator();
     }
 
+    [ExcludeFromCodeCoverage]
     public bool SupportsMultipleClients => throw new System.NotImplementedException();
 
+    [ExcludeFromCodeCoverage]
     public string GetClientName(OpenApiDocument document, string path, string httpMethod, OpenApiOperation operation)
     {
         return defaultGenerator.GetClientName(document, path, httpMethod, operation);
