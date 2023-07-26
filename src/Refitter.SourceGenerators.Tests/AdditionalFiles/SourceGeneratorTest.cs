ba60e7be1e61e518b2306662b25e42927612209b
Christian Helle <christian.helle@outlook.com>
2023-07-26 23:22:50 +0200

    Fix source generator tests

--- src/Refitter.SourceGenerators.Tests/AdditionalFiles/SourceGeneratorTest.cs
+++ src/Refitter.SourceGenerators.Tests/AdditionalFiles/SourceGeneratorTest.cs
@@ -1,5 +1,15 @@
-﻿namespace Refitter.Tests.AdditionalFiles;
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace Refitter.Tests.AdditionalFiles;
 
 public class SourceGeneratorTest
 {
+    [Fact]
+    public void Should_Type_Exist() =>
+        typeof(GeneratedCode.ISwaggerPetstore)
+            .Namespace
+            .Should()
+            .Be("GeneratedCode");
 }
