866b5a947f15b612e5a4ea056747a54578fd84b9
Christian Helle <christian.helle@outlook.com>
2023-08-01 16:05:21 +0200

    Implement IRefitInterfaceGenerator that can generate an interface per endpoint

---
+++ src/Refitter.Core/RefitMultipleInterfaceGenerator.cs
@@ -0,0 +1,123 @@
+using System.Text;
+
+using NSwag;
+
+namespace Refitter.Core;
+
+internal class RefitMultipleInterfaceGenerator : IRefitInterfaceGenerator
+{
+    private const string Separator = "    ";
+
+    private readonly RefitGeneratorSettings settings;
+    private readonly OpenApiDocument document;
+    private readonly CustomCSharpClientGenerator generator;
+
+    internal RefitMultipleInterfaceGenerator(
+        RefitGeneratorSettings settings,
+        OpenApiDocument document,
+        CustomCSharpClientGenerator generator)
+    {
+        this.settings = settings;
+        this.document = document;
+        this.generator = generator;
+        generator.BaseSettings.OperationNameGenerator = new OperationNameGenerator(document);
+    }
+
+    public string GenerateCode()
+    {
+        var code = new StringBuilder();
+        foreach (var kv in document.Paths)
+        {
+            foreach (var operations in kv.Value)
+            {
+                var operation = operations.Value;
+
+                var returnTypeParameter = new[] { "200", "201", "203", "206" }
+                    .Where(code => operation.Responses.ContainsKey(code))
+                    .Select(code => generator.GetTypeName(operation.Responses[code].ActualResponse.Schema, true, null))
+                    .FirstOrDefault();
+
+                var returnType = GetReturnType(returnTypeParameter);
+
+                var verb = operations.Key.CapitalizeFirstCharacter();
+
+                var name = generator.BaseSettings.OperationNameGenerator
+                    .GetOperationName(document, kv.Key, verb, operation);
+
+                GenerateInterfaceXmlDocComments(operation, code);
+                code.AppendLine($$"""
+                                  {{GenerateInterfaceDeclaration(name)}}
+                                  {{Separator}}{
+                                  """);
+
+                var operationModel = generator.CreateOperationModel(operation);
+                var parameters = ParameterExtractor.GetParameters(operationModel, operation, settings);
+                var parametersString = string.Join(", ", parameters);
+
+                GenerateMethodXmlDocComments(operation, code);
+
+                if (operationModel.Consumes.Contains("multipart/form-data"))
+                {
+                    code.AppendLine($"{Separator}{Separator}[Multipart]");
+                }
+
+                code.AppendLine($"{Separator}{Separator}[{verb}(\"{kv.Key}\")]")
+                    .AppendLine($"{Separator}{Separator}{returnType} Execute({parametersString});")
+                    .AppendLine($"{Separator}}}")
+                    .AppendLine();
+            }
+        }
+
+        return code.ToString();
+    }
+
+    private string GetReturnType(string? returnTypeParameter)
+    {
+        return returnTypeParameter is null or "void"
+            ? "Task"
+            : GetConfiguredReturnType(returnTypeParameter);
+    }
+
+    private string GetConfiguredReturnType(string returnTypeParameter)
+    {
+        return settings.ReturnIApiResponse
+            ? $"Task<IApiResponse<{WellKnownNamesspaces.TrimImportedNamespaces(returnTypeParameter)}>>"
+            : $"Task<{WellKnownNamesspaces.TrimImportedNamespaces(returnTypeParameter)}>";
+    }
+
+    private void GenerateInterfaceXmlDocComments(OpenApiOperation operation, StringBuilder code)
+    {
+        if (!settings.GenerateXmlDocCodeComments || 
+            string.IsNullOrWhiteSpace(operation.Summary))
+            return;
+
+        code.AppendLine(
+            $$"""
+              {{Separator}}/// <summary>
+              {{Separator}}/// {{operation.Summary}}
+              {{Separator}}/// </summary>
+              """);
+    }
+
+    private void GenerateMethodXmlDocComments(OpenApiOperation operation, StringBuilder code)
+    {
+        if (!settings.GenerateXmlDocCodeComments)
+            return;
+
+        if (!string.IsNullOrWhiteSpace(operation.Description))
+        {
+            code.AppendLine($"{Separator}{Separator}/// <summary>");
+
+            foreach (var line in operation.Description.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None))
+                code.AppendLine($"{Separator}{Separator}/// {line.Trim()}");
+
+            code.AppendLine($"{Separator}{Separator}/// </summary>");
+        }
+    }
+
+    private string GenerateInterfaceDeclaration(string name)
+    {
+        var modifier = settings.TypeAccessibility.ToString().ToLowerInvariant();
+        return $"{Separator}{modifier} interface I{name.CapitalizeFirstCharacter()}";
+    }
+}
