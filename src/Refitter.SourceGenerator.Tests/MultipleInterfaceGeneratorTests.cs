26f71ed5729d364413a05a61c8d12b38799d8e75
Christian Helle <christian.helle@outlook.com>
2023-08-12 15:45:33 +0200

    Refactor test files and adjust namespaces
    Deleted 'MultipleInterfaceGeneratorTests.cs', moved 'MultipleInterfaceByTagGeneratorTests.cs' and 'SingleInterfaceGeneratorTest.cs' from AdditionalFiles to the parent directory. Also, added a new file 'MultipleInterfaceGeneratorTests.cs' at the same level for symmetry and consistency.
    
    These changes are aimed to streamline the test directories and improve the logical grouping of test classes. This will make it easier to find and manage tests in the codebase.

---
+++ src/Refitter.SourceGenerator.Tests/MultipleInterfaceGeneratorTests.cs
@@ -0,0 +1,63 @@
+using FluentAssertions;
+
+using Refit;
+
+using Refitter.Tests.AdditionalFiles.ByEndpoint;
+
+using Xunit;
+
+namespace Refitter.SourceGenerators.Tests;
+
+public class MultipleInterfaceGeneratorTests
+{
+    [Theory]
+    [InlineData(typeof(IAddPetEndpoint))]
+    [InlineData(typeof(IUpdatePetEndpoint))]
+    [InlineData(typeof(IDeletePetEndpoint))]
+    [InlineData(typeof(ICreateUserEndpoint))]
+    [InlineData(typeof(IUpdateUserEndpoint))]
+    [InlineData(typeof(IDeleteUserEndpoint))]
+    [InlineData(typeof(ILoginUserEndpoint))]
+    [InlineData(typeof(ILogoutUserEndpoint))]
+    [InlineData(typeof(IPlaceOrderEndpoint))]
+    [InlineData(typeof(IGetOrderByIdEndpoint))]
+    [InlineData(typeof(IDeleteOrderEndpoint))]
+    [InlineData(typeof(IGetInventoryEndpoint))]
+    [InlineData(typeof(IGetPetByIdEndpoint))]
+    [InlineData(typeof(IUploadFileEndpoint))]
+    [InlineData(typeof(IFindPetsByStatusEndpoint))]
+    [InlineData(typeof(IFindPetsByTagsEndpoint))]
+    [InlineData(typeof(IGetUserByNameEndpoint))]
+    [InlineData(typeof(IUpdatePetWithFormEndpoint))]
+    [InlineData(typeof(ICreateUsersWithListInputEndpoint))]
+    public void Should_Generate_Interface(Type type) =>
+        type
+            .Namespace
+            .Should()
+            .Be("Refitter.Tests.AdditionalFiles.ByEndpoint");
+
+    [Theory]
+    [InlineData(typeof(IAddPetEndpoint))]
+    [InlineData(typeof(IUpdatePetEndpoint))]
+    [InlineData(typeof(IDeletePetEndpoint))]
+    [InlineData(typeof(ICreateUserEndpoint))]
+    [InlineData(typeof(IUpdateUserEndpoint))]
+    [InlineData(typeof(IDeleteUserEndpoint))]
+    [InlineData(typeof(ILoginUserEndpoint))]
+    [InlineData(typeof(ILogoutUserEndpoint))]
+    [InlineData(typeof(IPlaceOrderEndpoint))]
+    [InlineData(typeof(IGetOrderByIdEndpoint))]
+    [InlineData(typeof(IDeleteOrderEndpoint))]
+    [InlineData(typeof(IGetInventoryEndpoint))]
+    [InlineData(typeof(IGetPetByIdEndpoint))]
+    [InlineData(typeof(IUploadFileEndpoint))]
+    [InlineData(typeof(IFindPetsByStatusEndpoint))]
+    [InlineData(typeof(IFindPetsByTagsEndpoint))]
+    [InlineData(typeof(IGetUserByNameEndpoint))]
+    [InlineData(typeof(IUpdatePetWithFormEndpoint))]
+    [InlineData(typeof(ICreateUsersWithListInputEndpoint))]
+    public void Can_Resolve_Refit_Interface(Type type) =>
+        RestService.For(type, "https://petstore3.swagger.io/api/v3")
+            .Should()
+            .NotBeNull();
+}
