bca1dbac160abb921587716a316288e88a147544
Christian Helle <christian.helle@outlook.com>
2023-08-13 09:59:29 +0200

    Exclude Program.cs from code coverage
    Refactored the Program.cs by encapsulating the main functionality within a class and a namespace, improving code organization and readability. Also added an ExcludeFromCodeCoverage annotation, signaling to code coverage tools that this class should be ignored during analysis as it's a top-level program file not necessarily requiring unit tests.

--- src/Refitter/Program.cs
+++ src/Refitter/Program.cs
@@ -1,77 +1,87 @@
-﻿using Refitter;
+﻿using System.Diagnostics.CodeAnalysis;
+
 using Spectre.Console.Cli;
 
 
-Analytics.Configure();
+namespace Refitter;
 
-var app = new CommandApp<GenerateCommand>();
-app.Configure(
-    configuration =>
+[ExcludeFromCodeCoverage]
+internal class Program
+{
+    static int Main(string[] args)
     {
-        configuration
-            .SetApplicationName("refitter")
-            .SetApplicationVersion(typeof(GenerateCommand).Assembly.GetName().Version!.ToString());
+        Analytics.Configure();
 
-        configuration
-            .AddExample("./openapi.json");
+        var app = new CommandApp<GenerateCommand>();
+        app.Configure(
+            configuration =>
+            {
+                configuration
+                    .SetApplicationName("refitter")
+                    .SetApplicationVersion(typeof(GenerateCommand).Assembly.GetName().Version!.ToString());
 
-        configuration
-            .AddExample("https://petstore3.swagger.io/api/v3/openapi.yaml");
+                configuration
+                    .AddExample("./openapi.json");
 
-        configuration
-            .AddExample(
-                "./openapi.json",
-                "--namespace",
-                "\"Your.Namespace.Of.Choice.GeneratedCode\"",
-                "--output",
-                "./GeneratedCode.cs");
+                configuration
+                    .AddExample("https://petstore3.swagger.io/api/v3/openapi.yaml");
 
-        configuration
-            .AddExample(
-                "./openapi.json",
-                "--namespace",
-                "\"Your.Namespace.Of.Choice.GeneratedCode\"",
-                "--internal");
+                configuration
+                    .AddExample(
+                        "./openapi.json",
+                        "--namespace",
+                        "\"Your.Namespace.Of.Choice.GeneratedCode\"",
+                        "--output",
+                        "./GeneratedCode.cs");
 
-        configuration
-            .AddExample(
-                "./openapi.json",
-                "--output",
-                "./IGeneratedCode.cs",
-                "--interface-only");
+                configuration
+                    .AddExample(
+                        "./openapi.json",
+                        "--namespace",
+                        "\"Your.Namespace.Of.Choice.GeneratedCode\"",
+                        "--internal");
 
-        configuration
-            .AddExample(
-                "./openapi.json",
-                "--use-api-response");
+                configuration
+                    .AddExample(
+                        "./openapi.json",
+                        "--output",
+                        "./IGeneratedCode.cs",
+                        "--interface-only");
 
-        configuration
-            .AddExample(
-                "./openapi.json",
-                "--cancellation-tokens");
+                configuration
+                    .AddExample(
+                        "./openapi.json",
+                        "--use-api-response");
 
-        configuration
-            .AddExample(
-                "./openapi.json",
-                "--no-operation-headers");
+                configuration
+                    .AddExample(
+                        "./openapi.json",
+                        "--cancellation-tokens");
 
-        configuration
-            .AddExample(
-                "./openapi.json",
-                "--use-iso-date-format");
+                configuration
+                    .AddExample(
+                        "./openapi.json",
+                        "--no-operation-headers");
 
-        configuration
-            .AddExample(
-                "./openapi.json",
-                "--additional-namespace",
-                "\"Your.Additional.Namespace\"",
-                "--additional-namespace",
-                "\"Your.Other.Additional.Namespace\"");
+                configuration
+                    .AddExample(
+                        "./openapi.json",
+                        "--use-iso-date-format");
 
-        configuration
-            .AddExample(
-                "./openapi.json",
-                "--multiple-interfaces");
-    });
+                configuration
+                    .AddExample(
+                        "./openapi.json",
+                        "--additional-namespace",
+                        "\"Your.Additional.Namespace\"",
+                        "--additional-namespace",
+                        "\"Your.Other.Additional.Namespace\"");
 
-return app.Run(args);
+                configuration
+                    .AddExample(
+                        "./openapi.json",
+                        "--multiple-interfaces");
+            });
+
+        return app.Run(args);
+    }
+}
