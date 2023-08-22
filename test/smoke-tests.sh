25d5ebc7267a3a9ddfdbe1ab322da2eb379beff9
Christian Helle <crh@delegate.dk>
2023-08-22 08:54:50 +0200

    Update converted local smoke test script

--- test/smoke-tests.sh
+++ test/smoke-tests.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-Parallel=true
-
-function throw_on_failure {
+function ThrowOnNativeFailure {
     if [ $? -ne 0 ]; then
         echo "Native Failure"
         exit 1
     fi
 }
 
-function run_tests {
+function GenerateAndBuild {
+    format=$1
+    namespace=$2
+    outputPath=$3
+
+    find . -name '*.generated.cs' -delete
+
+    echo "dotnet run --no-build --project ../src/Refitter/Refitter.csproj ./openapi.$format --namespace $namespace --output ./GeneratedCode/$outputPath --no-logging"
+    dotnet run --no-build --project ../src/Refitter/Refitter.csproj ./openapi.$format --namespace $namespace --output ./GeneratedCode/$outputPath --no-logging
+    ThrowOnNativeFailure
+
+    echo -e "\nBuilding ConsoleApp\n"
+    dotnet build -p:TreatWarningsAsErrors=true ./ConsoleApp/ConsoleApp.sln
+    ThrowOnNativeFailure
+}
+
+function RunTests {
     method=$1
     parallel=$2
 
@@ -29,180 +43,27 @@ function run_tests {
         "hubspot-webhooks"
     )
 
-    versions=("v2.0" "v3.0")
-    formats=("json" "yaml")
+    echo "dotnet build --project ../src/Refitter/Refitter.csproj"
+    dotnet build -p:TreatWarningsAsErrors=true ../src/Refitter/Refitter.csproj
+    ThrowOnNativeFailure
 
-    for version in "${versions[@]}"; do
-        for format in "${formats[@]}"; do
+    for version in "v3.0" "v2.0"; do
+        for format in "json" "yaml"; do
             for filename in "${filenames[@]}"; do
                 filepath="./OpenAPI/$version/$filename.$format"
                 if [ -f "$filepath" ]; then
-                    cp "$filepath" "./openapi.$format"
-
-                    output_path="${filename^}.cs"
-                    namespace="${filename//-/}"
-                    namespace="${namespace^}"
-
-                    echo "dotnet run --project ../src/Refitter/Refitter.csproj ./openapi.$format --namespace $namespace --output ./GeneratedCode/$output_path --no-logging"
-                    dotnet run --project ../src/Refitter/Refitter.csproj "./openapi.$format" --namespace "$namespace" --output "./GeneratedCode/$output_path" --no-logging || throw_on_failure
-
-                    echo "dotnet run --project ../src/Refitter/Refitter.csproj ./openapi.$format --namespace ${namespace}.Cancellation --output ./GeneratedCode/WithCancellation$output_path --cancellation-tokens --no-logging"
-                    dotnet run --project ../src/Refitter/Refitter.csproj "./openapi.$format" --namespace "${namespace}.Cancellation" --output "./GeneratedCode/WithCancellation$output_path" --cancellation-tokens --no-logging || throw_on_failure
-
-                    echo "dotnet run --project ../src/Refitter/Refitter.csproj ./openapi.$format --namespace ${namespace}.Internal --output ./GeneratedCode/Internal$output_path --internal --no-logging"
-                    dotnet run --project ../src/Refitter/Refitter.csproj "./openapi.$format" --namespace "${namespace}.Internal" --output "./GeneratedCode/Internal$output_path" --internal --no-logging || throw_on_failure
-
-                    echo "dotnet run --project ../src/Refitter/Refitter.csproj ./openapi.$format --namespace ${namespace}.Interface --output ./GeneratedCode/I$output_path --interface-only --no-logging"
-                    dotnet run --project ../src/Refitter/Refitter.csproj "./openapi.$format" --namespace "${namespace}.Interface" --output "./GeneratedCode/I$output_path" --interface-only --no-logging || throw_on_failure
-
-                    echo "dotnet run --project ../src/Refitter/Refitter.csproj ./openapi.$format --namespace ${namespace}.UsingApiResponse --output ./GeneratedCode/IApi$output_path --use-api-response --interface-only --no-logging"
-                    dotnet run --project ../src/Refitter/Refitter.csproj "./openapi.$format" --namespace "${namespace}.UsingApiResponse" --output "./GeneratedCode/IApi$output_path" --use-api-response --interface-only --no-logging || throw_on_failure
-
-                    echo "dotnet run --project ../src/Refitter/Refitter.csproj ./openapi.$format --namespace ${namespace}.UsingIsoDateFormat --output ./GeneratedCode/UsingIsoDateFormat$output_path --use-iso-date-format --no-logging"
-                    dotnet run --project ../src/Refitter/Refitter.csproj "./openapi.$format" --namespace "${namespace}.UsingIsoDateFormat" --output "./GeneratedCode/UsingIsoDateFormat$output_path" --use-iso-date-format --no-logging || throw_on_failure
-
-                    echo "dotnet run --project ../src/Refitter/Refitter.csproj ./openapi.$format --namespace ${namespace}.MultipleInterfaces --output ./GeneratedCode/MultipleInterfaces$output_path --multiple-interfaces --no-logging"
-                    dotnet run --project ../src/Refitter/Refitter.csproj "./openapi.$format" --namespace "${namespace}.MultipleInterfaces" --output "./GeneratedCode/MultipleInterfaces$output_path" --multiple-interfaces byEndpoint --no-logging || throw_on_failure
-
-                    cp "./GeneratedCode/$output_path" "./$version-$filename-$format.cs"
-                    cp "./GeneratedCode/$output_path" "./ConsoleApp/Net7/" -f
-                    cp "./GeneratedCode/$output_path" "./ConsoleApp/Net6/" -f
-                    cp "./GeneratedCode/$output_path" "./ConsoleApp/Net48/" -f
-                    cp "./GeneratedCode/$output_path" "./ConsoleApp/Net481/" -f
-                    cp "./GeneratedCode/$output_path" "./ConsoleApp/Net472/" -f
-                    cp "./GeneratedCode/$output_path" "./ConsoleApp/Net462/" -f
-                    cp "./GeneratedCode/$output_path" "./ConsoleApp/NetStandard20/" -f
-                    cp "./GeneratedCode/$output_path" "./ConsoleApp/NetStandard21/" -f
-                    cp "./GeneratedCode/$output_path" "./MinimalApi/" -f
-                    cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net7/" -f
-                    cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net6/" -f
-                    cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net48/" -f
-                    cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net481/" -f
-                    cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net472/" -f
-                    cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net462/" -f
-                    cp "./GeneratedCode/I$output_path" "./ConsoleApp/NetStandard20/" -f
-                    cp "./GeneratedCode/I$output_path" "./ConsoleApp/NetStandard21/" -f
-                    cp "./GeneratedCode/I$output_path" "./MinimalApi/" -f
-                    cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net7/" -f
-                    cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net6/" -f
-                    cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net48/" -f
-                    cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net481/" -f
-                    cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net472/" -f
-                    cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net462/" -f
-                    cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/NetStandard20/" -f
-                    cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/NetStandard21/" -f
-                    cp "./GeneratedCode/IApi$output_path" "./MinimalApi/" -f
-                    cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net7/" -f
-                    cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net6/" -f
-                    cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net48/" -f
-                    cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net481/" -f
-                    cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net472/" -f
-                    cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net462/" -f
-                    cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/NetStandard20/" -f
-                    cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/NetStandard21/" -f
-                    cp "./GeneratedCode/Internal$output_path" "./MinimalApi/" -f
-                    cp "./GeneratedCode/WithCancellation$output_path" "./ConsoleApp/Net7/" -f
-                    cp "./GeneratedCode/WithCancellation$output_path" "./ConsoleApp/Net6/" -f
-                    cp "./GeneratedCode/WithCancellation$output_path" "./ConsoleApp/Net48/" -f
-                    cp "./GeneratedCode/WithCancellation$output_path" "./ConsoleApp/Net481/" -f
-                    cp "./GeneratedCode/WithCancellation$output_path" "./ConsoleApp/Net472/" -f
-                    cp "./GeneratedCode/WithCancellation$output_path" "./ConsoleApp/Net462/" -f
-                    cp "./GeneratedCode/WithCancellation$output_path" "./ConsoleApp/NetStandard20/" -f
-                    cp "./GeneratedCode/WithCancellation$output_path" "./ConsoleApp/NetStandard21/" -f
-                    cp "./GeneratedCode/WithCancellation$output_path" "./MinimalApi/" -f
-                    cp "./GeneratedCode/UsingIsoDateFormat$output_path" "./ConsoleApp/Net7/" -f
-                    cp "./GeneratedCode/UsingIsoDateFormat$output_path" "./ConsoleApp/Net6/" -f
-                    cp "./GeneratedCode/UsingIsoDateFormat$output_path" "./ConsoleApp/Net48/" -f
-                    cp "./GeneratedCode/UsingIsoDateFormat$output_path" "./ConsoleApp/Net481/" -f
-                    cp "./GeneratedCode/UsingIsoDateFormat$output_path" "./ConsoleApp/Net472/" -f
-                    cp "./GeneratedCode/UsingIsoDateFormat$output_path" "./ConsoleApp/Net462/" -f
-                    cp "./GeneratedCode/UsingIsoDateFormat$output_path" "./ConsoleApp/NetStandard20/" -f
-                    cp "./GeneratedCode/UsingIsoDateFormat$output_path" "./ConsoleApp/NetStandard21/" -f
-                    cp "./GeneratedCode/UsingIsoDateFormat$output_path" "./MinimalApi/" -f
-                    cp "./GeneratedCode/MultipleInterfaces$output_path" "./ConsoleApp/Net7/" -f
-                    cp "./GeneratedCode/MultipleInterfaces$output_path" "./ConsoleApp/Net6/" -f
-                    cp "./GeneratedCode/MultipleInterfaces$output_path" "./ConsoleApp/Net48/" -f
-                    cp "./GeneratedCode/MultipleInterfaces$output_path" "./ConsoleApp/Net481/" -f
-                    cp "./GeneratedCode/MultipleInterfaces$output_path" "./ConsoleApp/Net472/" -f
-                    cp "./GeneratedCode/MultipleInterfaces$output_path" "./ConsoleApp/Net462/" -f
-                    cp "./GeneratedCode/MultipleInterfaces$output_path" "./ConsoleApp/NetStandard20/" -f
-                    cp "./GeneratedCode/MultipleInterfaces$output_path" "./ConsoleApp/NetStandard21/" -f
-                    cp "./GeneratedCode/MultipleInterfaces$output_path" "./MinimalApi/" -f
+                    cp "$filepath" ./openapi.$format
+                    outputPath="$(tr '[:lower:]' '[:upper:]' <<< ${filename:0:1})${filename:1}.generated.cs"
+                    namespace="$(tr '-' '' <<< ${filename})"
+                    namespace="$(tr '[:lower:]' '[:upper:]' <<< ${namespace:0:1})${namespace:1}"
+                    GenerateAndBuild $format $namespace $outputPath
+                    GenerateAndBuild $format "${namespace}.Cancellation" "WithCancellation$outputPath"
+                    GenerateAndBuild $format "${namespace}.Internal" "Internal$outputPath"
+                    GenerateAndBuild $format "${namespace}.Interface" "I$outputPath"
                 fi
             done
-
-            echo -e "\nBuilding ConsoleApp\n"
-            dotnet build "./ConsoleApp/ConsoleApp.sln" || throw_on_failure
-
-            echo -e "\nBuilding MinimalApi\n"
-            dotnet build "./MinimalApi/MinimalApi.csproj" || throw_on_failure
         done
-    done
-
-    uris=(
-        "https://petstore3.swagger.io/api/v3/openapi.json"
-        "https://petstore3.swagger.io/api/v3/openapi.yaml"
-    )
-    namespace="PetstoreFromUri"
-    output_path="PetstoreFromUri.cs"
-
-    for uri in "${uris[@]}"; do
-        echo "dotnet run --project ../src/Refitter/Refitter.csproj \"$uri\" --namespace $namespace --output ./GeneratedCode/$output_path"
-        dotnet run --project ../src/Refitter/Refitter.csproj "$uri" --namespace "$namespace" --output "./GeneratedCode/$output_path" || throw_on_failure
-
-        echo "dotnet run --project ../src/Refitter/Refitter.csproj \"$uri\" --namespace ${namespace}.Internal --output ./GeneratedCode/Internal$output_path --internal --no-logging"
-        dotnet run --project ../src/Refitter/Refitter.csproj "$uri" --namespace "${namespace}.Internal" --output "./GeneratedCode/Internal$output_path" --internal --no-logging || throw_on_failure
-
-        echo "dotnet run --project ../src/Refitter/Refitter.csproj \"$uri\" --namespace ${namespace}.Interface --output ./GeneratedCode/I$output_path --interface-only --no-logging"
-        dotnet run --project ../src/Refitter/Refitter.csproj "$uri" --namespace "${namespace}.Interface" --output "./GeneratedCode/I$output_path" --interface-only --no-logging || throw_on_failure
-
-        echo "dotnet run --project ../src/Refitter/Refitter.csproj \"$uri\" --namespace ${namespace}.UsingApiResponse --output ./GeneratedCode/IApi$output_path --use-api-response --interface-only --no-logging"
-        dotnet run --project ../src/Refitter/Refitter.csproj "$uri" --namespace "${namespace}.UsingApiResponse" --output "./GeneratedCode/IApi$output_path" --use-api-response --interface-only --no-logging || throw_on_failure
-
-        cp "./GeneratedCode/$output_path" "./ConsoleApp/Net7/" -f
-        cp "./GeneratedCode/$output_path" "./ConsoleApp/Net6/" -f
-        cp "./GeneratedCode/$output_path" "./ConsoleApp/Net48/" -f
-        cp "./GeneratedCode/$output_path" "./ConsoleApp/Net481/" -f
-        cp "./GeneratedCode/$output_path" "./ConsoleApp/Net472/" -f
-        cp "./GeneratedCode/$output_path" "./ConsoleApp/Net462/" -f
-        cp "./GeneratedCode/$output_path" "./ConsoleApp/NetStandard20/" -f
-        cp "./GeneratedCode/$output_path" "./ConsoleApp/NetStandard21/" -f
-        cp "./GeneratedCode/$output_path" "./MinimalApi/" -f
-        cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net7/" -f
-        cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net6/" -f
-        cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net48/" -f
-        cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net481/" -f
-        cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net472/" -f
-        cp "./GeneratedCode/I$output_path" "./ConsoleApp/Net462/" -f
-        cp "./GeneratedCode/I$output_path" "./ConsoleApp/NetStandard20/" -f
-        cp "./GeneratedCode/I$output_path" "./ConsoleApp/NetStandard21/" -f
-        cp "./GeneratedCode/I$output_path" "./MinimalApi/" -f
-        cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net7/" -f
-        cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net6/" -f
-        cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net48/" -f
-        cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net481/" -f
-        cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net472/" -f
-        cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/Net462/" -f
-        cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/NetStandard20/" -f
-        cp "./GeneratedCode/IApi$output_path" "./ConsoleApp/NetStandard21/" -f
-        cp "./GeneratedCode/IApi$output_path" "./MinimalApi/" -f
-        cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net7/" -f
-        cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net6/" -f
-        cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net48/" -f
-        cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net481/" -f
-        cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net472/" -f
-        cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/Net462/" -f
-        cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/NetStandard20/" -f
-        cp "./GeneratedCode/Internal$output_path" "./ConsoleApp/NetStandard21/" -f
-        cp "./GeneratedCode/Internal$output_path" "./MinimalApi/" -f
-
-        echo -e "\nBuilding ConsoleApp\n"
-        dotnet build "./ConsoleApp/ConsoleApp.sln" || throw_on_failure
-
-        echo -e "\nBuilding MinimalApi\n"
-        dotnet build "./MinimalApi/MinimalApi.csproj" || throw_on_failure
     done
 }
 
-run_tests -method "dotnet-run" -parallel "$parallel"
-echo -e "\n"
+RunTests "dotnet-run" false
