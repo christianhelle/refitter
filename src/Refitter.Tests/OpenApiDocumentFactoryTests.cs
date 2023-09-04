42cdc1b3c969e3b45f112e9ed52b15552aa3c8b8
Christian Helle <christian.helle@outlook.com>
2023-09-04 11:48:21 +0200

    Add tests for OpenApiDocumentFactory
    This commit introduces unit tests for `OpenApiDocumentFactory` in `OpenApiDocumentFactoryTests.cs`. The tests ensure that generated code can be built from a given URL and confirm code generation with multiple interfaces. Additionally, a new method `CreateSwaggerFile` in `TestFile.cs` is added to create temporary Swagger files for testing purposes. This is necessary to validate code generation against different OpenApi specifications.

---
+++ src/Refitter.Tests/OpenApiDocumentFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+
+using Refitter.Core;
+using Refitter.Tests.Resources;
+
+using Xunit;
+
+namespace Refitter.Tests;
+
+public class OpenApiDocumentFactoryTests
+{
+    [Theory]
+    [InlineData("https://developers.intellihr.io/docs/v1/swagger.json")] // GZIP encoded
+    [InlineData("http://raw.githubusercontent.com/christianhelle/refitter/main/test/OpenAPI/v3.0/petstore.json")]
+    public async Task Can_Build_Generated_Code_From_Url(string url)
+    {
+        var settings = new RefitGeneratorSettings { OpenApiPath = url };
+        (await OpenApiDocumentFactory.CreateAsync(settings))
+            .Should()
+            .NotBeNull();
+    }
+    
+    [Theory]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV3, "SwaggerPetstore.json")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV3, "SwaggerPetstore.yaml")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreJsonV2, "SwaggerPetstore.json")]
+    [InlineData(SampleOpenSpecifications.SwaggerPetstoreYamlV2, "SwaggerPetstore.yaml")]
+    public async Task Can_Generate_Code_With_Multiple_Interfaces(SampleOpenSpecifications version, string filename)
+    {
+        var swaggerFile = await TestFile.CreateSwaggerFile(EmbeddedResources.GetSwaggerPetstore(version), filename);
+        var settings = new RefitGeneratorSettings { OpenApiPath = swaggerFile };
+        (await OpenApiDocumentFactory.CreateAsync(settings))
+            .Should()
+            .NotBeNull();
+    }
+}
