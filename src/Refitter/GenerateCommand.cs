f682b2190ad8a51f44cd54f833b4f03a412c236b
Christian Helle <christian.helle@outlook.com>
2023-09-21 00:35:14 +0200

    Add support for optional nullable parameters in GenerateCommand
    This commit includes a change to the GenerateCommand where it now takes into account optional nullable parameters. This added functionality allows for more flexibility when dealing with nullable parameters. The ability to mark parameters as optional and nullable can help in scenarios where the parameter might not always be required.

--- src/Refitter/GenerateCommand.cs
+++ src/Refitter/GenerateCommand.cs
@@ -77,6 +77,7 @@ public sealed class GenerateCommand : AsyncCommand<Settings>
             IncludeTags = settings.Tags ?? Array.Empty<string>(),
             GenerateDeprecatedOperations = !settings.NoDeprecatedOperations,
             OperationNameTemplate = settings.OperationNameTemplate,
+            OptionalParameters = settings.OptionalNullableParameters
         };
 
         try
