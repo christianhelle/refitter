42532933d6943289416884e61f49a2ffb0be51b8
Christian Helle <christian.helle@outlook.com>
2023-09-20 09:42:46 +0200

    Add OptionalParametersGenerator tests
    This commit introduces tests for the OptionalParametersGenerator in the Refitter.SourceGenerator.Tests project. The tests - 'Should_Type_Exist' and 'Can_Resolve_Refit_Interface' - check that the type exists within its correct namespace and tests if the interface can be successfully resolved, respectively. This ensures the functionality of optional parameters in generated calls.

---
+++ src/Refitter.SourceGenerator.Tests/OptionalParametersGeneratorTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+
+using Refit;
+
+using Refitter.Tests.AdditionalFiles.OptionalParameters;
+
+using Xunit;
+
+namespace Refitter.SourceGenerators.Tests;
+
+public class OperationalParametersGeneratorTest
+{
+    [Fact]
+    public void Should_Type_Exist() =>
+        typeof(ISwaggerPetstoreWithOptionalParameters)
+            .Namespace
+            .Should()
+            .Be("Refitter.Tests.AdditionalFiles.OptionalParameters");
+
+    [Fact]
+    public void Can_Resolve_Refit_Interface() =>
+        RestService.For<ISwaggerPetstoreWithOptionalParameters>("https://petstore3.swagger.io/api/v3")
+            .Should()
+            .NotBeNull();
+}
