7022741a21c080d8c9c052c28cbd77ef2fe28c9c
Christian Helle <christian.helle@outlook.com>
2023-10-04 21:28:52 +0200

    Add JsonProperty attributes to DependencyInjectionSettings

--- src/Refitter.Core/RefitGeneratorSettings.cs
+++ src/Refitter.Core/RefitGeneratorSettings.cs
@@ -215,26 +215,36 @@ public class DependencyInjectionSettings
     /// <summary>
     /// Base Address for the HttpClient
     /// </summary>
+    [JsonPropertyName("baseUrl")]
+    [JsonProperty("baseUrl")]
     public string BaseUrl { get; set; }
 
     /// <summary>
     /// A collection of HttpMessageHandlers to be added to the HttpClient pipeline.
     /// This can be for telemetry logging, authorization, etc.
     /// </summary>
+    [JsonPropertyName("httpMessageHandlers")]
+    [JsonProperty("httpMessageHandlers")]
     public string[] HttpMessageHandlers { get; set; } = Array.Empty<string>();
 
     /// <summary>
     /// Set this to true to use Polly for transient fault handling.
     /// </summary>
+    [JsonPropertyName("usePolly")]
+    [JsonProperty("usePolly")]
     public bool UsePolly { get; set; }
 
     /// <summary>
     /// Default max retry count for Polly. Default is 6.
     /// </summary>
+    [JsonPropertyName("pollyMaxRetryCount")]
+    [JsonProperty("pollyMaxRetryCount")]
     public int PollyMaxRetryCount { get; set; } = 6;
 
     /// <summary>
     /// The median delay to target before the first retry in seconds. Default is 1 second
     /// </summary>
+    [JsonPropertyName("firstBackoffRetryInSeconds")]
+    [JsonProperty("firstBackoffRetryInSeconds")]
     public int FirstBackoffRetryInSeconds { get; set; } = 1;
 }
