d7778945ec07da4b781eabd34b0d61c9b96b5c5a
Christian Helle <christian.helle@outlook.com>
2023-10-04 16:31:59 +0200

    Introduce DependencyInjectionGenerator and its tests
    This commit includes the implementation of a new DependencyInjectionGenerator class which is used to generate Dependency Injection configurations for Refit clients in a service collection. Furthermore, we also added tests to verify the code produced by the generator. This change makes it easier to set up Refit clients with built-in Polly support and multiple HttpMessageHandlers.

---
+++ src/Refitter.Core/DependencyInjectionGenerator.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+
+namespace Refitter.Core;
+
+public class DependencyInjectionGenerator
+{
+    public static string Generate(
+        RefitGeneratorSettings settings,
+        IEnumerable<string> interfaceNames)
+    {
+        var iocSettings = settings.DependencyInjectionSettings;
+        if (iocSettings is null)
+            return string.Empty;
+
+        var code = new StringBuilder();
+
+        code.AppendLine();
+        code.AppendLine("using Microsoft.Extensions.DependencyInjection;");
+
+        if (iocSettings.UsePolly)
+        {
+            code.AppendLine(
+                """
+                using Polly;
+                using Polly.Contrib.WaitAndRetry;
+                using Polly.Extensions.Http;
+                """);
+        }
+
+        code.AppendLine();
+        code.AppendLine(
+            $$"""
+              namespace {{settings.Namespace}}
+              {
+                  public static class ServiceCollectionExtensions
+                  {
+                      public static IServiceCollection ConfigureRefitClients(this IServiceCollection services)
+                      {
+              """);
+        foreach (var interfaceName in interfaceNames)
+        {
+            code.Append(
+                $$"""
+                              services
+                                  .AddRefitClient<{{interfaceName}}>()
+                                  .ConfigureHttpClient(c => c.BaseAddress = new Uri({{iocSettings.BaseUrl}}))
+                  """);
+
+            foreach (string httpMessageHandler in iocSettings.HttpMessageHandlers)
+            {
+                code.AppendLine();
+                code.Append($"                .AddHttpMessageHandler<{httpMessageHandler}>()");
+            }
+
+            if (iocSettings.UsePolly)
+            {
+                code.AppendLine();
+                code.Append(
+                    $$"""
+                                      .AddPolicyHandler(
+                                          HttpPolicyExtensions
+                                              .HandleTransientHttpError()
+                                              .WaitAndRetryAsync(
+                                                  Backoff.DecorrelatedJitterBackoffV2(
+                                                      TimeSpan.FromSeconds({{iocSettings.FirstBackoffRetryInSeconds}}),
+                                                      {{iocSettings.PollyMaxRetryCount}}))
+                      """);
+            }
+
+            code.Append(");");
+            code.AppendLine();
+            code.AppendLine();
+        }
+        
+        code.Remove(code.Length - 3, 3);
+        code.AppendLine("        }");
+        code.AppendLine("    }");
+        code.AppendLine("}");
+        return code.ToString();
+    }
+}
---
+++ src/Refitter.Tests/DependencyInjectionGeneratorTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+
+using Refitter.Core;
+
+using Xunit;
+
+namespace Refitter.Tests;
+
+public class DependencyInjectionGeneratorTests
+{
+    private readonly RefitGeneratorSettings settings = new()
+    {
+        DependencyInjectionSettings = new DependencyInjectionSettings
+        {
+            BaseUrl = "https://petstore3.swagger.io/api/v3",
+            HttpMessageHandlers = new[]
+            {
+                "AuthorizationMessageHandler",
+                "DiagnosticMessageHandler"
+            },
+            UsePolly = true
+        }
+    };
+
+    [Fact]
+    public void Can_Generate_For_Multiple_Interfaces()
+    {
+        string code = DependencyInjectionGenerator.Generate(
+            settings,
+            new[]
+            {
+                "IPetApi",
+                "IStoreApi"
+            });
+        
+        code.Should().Contain("AddRefitClient<IPetApi>()");
+        code.Should().Contain("AddRefitClient<IStoreApi>()");
+    }
+
+    [Fact]
+    public void Can_Generate_With_HttpMessageHandlers()
+    {
+        string code = DependencyInjectionGenerator.Generate(
+            settings,
+            new[]
+            {
+                "IPetApi",
+                "IStoreApi"
+            });
+        
+        code.Should().Contain("AddHttpMessageHandler<AuthorizationMessageHandler>()");
+        code.Should().Contain("AddHttpMessageHandler<DiagnosticMessageHandler>()");
+    }
+
+    [Fact]
+    public void Can_Generate_With_Polly()
+    {
+        string code = DependencyInjectionGenerator.Generate(
+            settings,
+            new[]
+            {
+                "IPetApi",
+                "IStoreApi"
+            });
+        
+        code.Should().Contain("AddPolicyHandler");
+        code.Should().Contain("Backoff.DecorrelatedJitterBackoffV2");
+    }
+}
