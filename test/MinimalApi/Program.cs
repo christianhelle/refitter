249e354158fd0aade1cce9a49fa7f91961f0978a
Christian Helle <christian.helle@outlook.com>
2023-10-06 00:33:38 +0200

    Update test MinimalApi project
    A new class `TelemetryDelegatingHandler` is added to track outbound HTTP requests. It logs every outbound HTTP request along with its method and URL. It is registered as a transient dependency and will be added to the HTTP handlers via the `AddRefitClient` extension method. Changes have also been made in the `MinimalApi.csproj` and `Program.cs` files to reflect this update. Telemetry can be very useful to gain insights, especially about bottlenecks and to improve performance. Newline issue at the end of the files is also addressed.
    
    [skip ci]

--- test/MinimalApi/Program.cs
+++ test/MinimalApi/Program.cs
@@ -4,9 +4,10 @@ using Refit;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services
-    .AddRefitClient<ISwaggerPetstore>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://petstore3.swagger.io/api/v3"));
+
+builder.Services.AddLogging(logging => logging.AddDebug());
+builder.Services.AddTransient<TelemetryDelegatingHandler>();
+builder.Services.ConfigureRefitClients();
 
 var app = builder.Build();
 app.MapGet(
@@ -29,3 +30,4 @@ app.UseHttpsRedirection();
 app.UseSwaggerUI();
 app.UseSwagger();
 app.Run();
+
