95c67f3229393d5824c4a2a63b45b1af9fc85ab3
Christian Helle <christian.helle@outlook.com>
2023-10-09 12:03:53 +0200

    Add missing XML docs for MultipleInterfaces [skip ci]

--- src/Refitter.Core/RefitGeneratorSettings.cs
+++ src/Refitter.Core/RefitGeneratorSettings.cs
@@ -163,14 +163,28 @@ public class RefitGeneratorSettings
     public DependencyInjectionSettings? DependencyInjectionSettings { get; set; }
 }
 
+/// <summary>
+/// Enum representing the different options for generating multiple Refit interfaces.
+/// </summary>
 public enum MultipleInterfaces
 {
+    /// <summary>
+    /// Do not generate multiple interfaces
+    /// </summary>
     [JsonPropertyName("unset")]
     [JsonProperty("unset")]
     Unset,
+    /// <summary>
+    /// Generate a Refit interface for each endpoint with a single Execute() method. 
+    /// The method name can be customized using the --operation-name-template command line option, 
+    /// or the operationNameTemplate property in the settings file.
+    /// </summary>
     [JsonPropertyName("byEndpoint")]
     [JsonProperty("byEndpoint")]
     ByEndpoint,
+    /// <summary>
+    /// Generate a Refit interface for each tag
+    /// </summary>
     [JsonPropertyName("byTag")]
     [JsonProperty("byTag")]
     ByTag
