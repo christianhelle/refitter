d172807fe402c9a3fc6f5d4d5c10b485a5e0c8ba
Christian Helle <christian.helle@outlook.com>
2023-10-10 13:05:49 +0200

    Enhanced build error tracking in BuildHelper
    Modified BuildHelper to capture and report build output and errors more effectively. The changes follow best practices for using System.Diagnostics.Process, such as redirecting output and error streams, and includes a new exception type for build failures. This allows for better tracking and diagnosis of issues, especially in automated builds.

--- src/Refitter.Tests/Build/BuildHelper.cs
+++ src/Refitter.Tests/Build/BuildHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text;
 
 namespace Refitter.Tests.Build;
 
@@ -11,15 +12,44 @@ public static class BuildHelper
         var projectFile = Path.Combine(path, "Project.csproj");
         File.WriteAllText(projectFile, ProjectFileContents.Net70App);
         File.WriteAllText(Path.Combine(path, "Generated.cs"), generatedCode);
-        var process = Process.Start(GetDotNetCli(), $"build \"{projectFile}\"");
+
+        var processStartInfo = new ProcessStartInfo(GetDotNetCli(), $"build \"{projectFile}\"");
+        processStartInfo.RedirectStandardOutput = true;
+        processStartInfo.RedirectStandardError = true;
+        processStartInfo.UseShellExecute = false;
+        processStartInfo.CreateNoWindow = true;
+
+        var process = new Process();
+        process.StartInfo = processStartInfo;
+        
+        var output = new StringBuilder();
+        process.OutputDataReceived += (_, args) => output.AppendLine(args.Data);
+        
+        var errors = new StringBuilder();
+        process.ErrorDataReceived += (_, args) => errors.AppendLine(args.Data);
+
+        bool startResult = process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
         process.WaitForExit();
-        return process.ExitCode == 0;
+
+        var result = startResult && process.ExitCode == 0;
+        if (!result)
+            throw new BuildFailedException(errors.ToString(), output.ToString());
+
+        return result;
     }
 
     private static string GetDotNetCli()
     {
-        return Environment.OSVersion.Platform is PlatformID.Unix or PlatformID.MacOSX 
-            ? "dotnet" 
+        return Environment.OSVersion.Platform is PlatformID.Unix or PlatformID.MacOSX
+            ? "dotnet"
             : "dotnet.exe";
     }
+}
+
+public class BuildFailedException(string Errors, string Output)
+    : Exception($"Build failed{Crlf}Errors:{Crlf}{Errors}{Crlf}Output:{Crlf}{Output}")
+{
+    private static readonly string Crlf = Environment.NewLine;
 }
