63da2d2b66c6049e51fe50f02dfa4089b042b49b
Christian Helle <christian.helle@outlook.com>
2023-10-11 22:03:23 +0200

    Add support for customizing the NSwag CSharpGeneratorSettings from CSharpClientGeneratorFactory

---
+++ src/Refitter.Tests/CustomCSharpGeneratorSettingsTests.cs
@@ -0,0 +1,80 @@
+using System.Dynamic;
+
+using Atc.Test;
+
+using FluentAssertions;
+
+using NJsonSchema.CodeGeneration.CSharp;
+
+using Refitter.Core;
+using Refitter.Tests.Resources;
+
+using Xunit;
+
+namespace Refitter.Tests;
+
+public class CustomCSharpGeneratorSettingsTests
+{
+    [Theory]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreJsonV3, "SwaggerPetstore.json")]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreYamlV3, "SwaggerPetstore.yaml")]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreJsonV2, "SwaggerPetstore.json")]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreYamlV2, "SwaggerPetstore.yaml")]
+    public async Task Can_Generate_Custom_DateType(
+        SampleOpenSpecifications version,
+        string filename)
+    {
+        var settings = new RefitGeneratorSettings();
+        settings.ContractsGeneratorSettings = new CSharpGeneratorSettings();
+        settings.ContractsGeneratorSettings!.DateType = "DateTime";
+        var generateCode = await GenerateCode(version, filename, settings);
+        generateCode.Should().NotBeNullOrWhiteSpace();
+        generateCode.Should().Contain("DateTime");
+    }
+    
+    [Theory]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreJsonV3, "SwaggerPetstore.json")]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreYamlV3, "SwaggerPetstore.yaml")]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreJsonV2, "SwaggerPetstore.json")]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreYamlV2, "SwaggerPetstore.yaml")]
+    public async Task Can_Generate_Custom_DateTimeType(
+        SampleOpenSpecifications version,
+        string filename)
+    {
+        var settings = new RefitGeneratorSettings();
+        settings.ContractsGeneratorSettings = new CSharpGeneratorSettings();
+        settings.ContractsGeneratorSettings!.DateTimeType = "DateTime";
+        var generateCode = await GenerateCode(version, filename, settings);
+        generateCode.Should().NotBeNullOrWhiteSpace();
+        generateCode.Should().Contain("DateTime");
+    }
+    
+    [Theory]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreJsonV3, "SwaggerPetstore.json")]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreYamlV3, "SwaggerPetstore.yaml")]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreJsonV2, "SwaggerPetstore.json")]
+    [InlineAutoNSubstituteData(SampleOpenSpecifications.SwaggerPetstoreYamlV2, "SwaggerPetstore.yaml")]
+    public async Task Can_Generate_Custom_ArrayType(
+        SampleOpenSpecifications version,
+        string filename)
+    {
+        var settings = new RefitGeneratorSettings();
+        settings.ContractsGeneratorSettings = new CSharpGeneratorSettings();
+        settings.ContractsGeneratorSettings!.ArrayType = "System.Collection.Generic.IList";
+        var generateCode = await GenerateCode(version, filename, settings);
+        generateCode.Should().NotBeNullOrWhiteSpace();
+        generateCode.Should().Contain("System.Collection.Generic.IList<");
+    }
+
+    private static async Task<string> GenerateCode(
+        SampleOpenSpecifications version,
+        string filename,
+        RefitGeneratorSettings settings)
+    {
+        var swaggerFile = await TestFile.CreateSwaggerFile(EmbeddedResources.GetSwaggerPetstore(version), filename);
+        settings.OpenApiPath = swaggerFile;
+
+        var sut = await RefitGenerator.CreateAsync(settings);
+        return sut.Generate();
+    }
+}
