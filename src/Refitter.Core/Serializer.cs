f220ea894d64652c1cc777108b49f5b935e7a7a7
Christian Helle <christian.helle@outlook.com>
2023-10-12 00:58:17 +0200

    Update Serializer.cs with comments and remove CamelCase naming
    The JsonNamingPolicy.CamelCase was removed from JsonSerializerOptions since the case-insensitivity setting simplifies our JSON handling. Detailed comments have also been added to improve readability and code understanding.

--- src/Refitter.Core/Serializer.cs
+++ src/Refitter.Core/Serializer.cs
@@ -1,21 +1,34 @@
 using System.Text.Json;
 
-using JsonNamingPolicy = System.Text.Json.JsonNamingPolicy;
 using JsonSerializerOptions = System.Text.Json.JsonSerializerOptions;
 
 namespace Refitter.Core;
 
+/// <summary>
+/// Provides methods for serializing and deserializing objects to and from JSON.
+/// This serializer is configured to be case-insensitive.
+/// </summary>
 public static class Serializer
 {
     private static readonly JsonSerializerOptions JsonSerializerOptions = new()
     {
-        PropertyNameCaseInsensitive = true,
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        PropertyNameCaseInsensitive = true
     };
 
+    /// <summary>
+    /// Deserializes the JSON string to the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type to deserialize the JSON string to.</typeparam>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized object of type T.</returns>
     public static T Deserialize<T>(string json) => 
         JsonSerializer.Deserialize<T>(json, JsonSerializerOptions)!;
 
+    /// <summary>
+    /// Serializes the specified object to a JSON string.
+    /// </summary>
+    /// <param name="any">The object to serialize.</param>
+    /// <returns>The JSON string representation of the object.</returns>
     public static string Serialize(object any) => 
         JsonSerializer.Serialize(any, JsonSerializerOptions);
 }
