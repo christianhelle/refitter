fb486700850614602c661361cb993f4b6abde295
Christian Helle <christian.helle@outlook.com>
2023-10-11 23:14:13 +0200

    Split settings into respective classes for better maintainability
    Refactored `RefitGeneratorSettings.cs` into different classes, each encapsulating settings related to distinct functionalities such as `NSwagCodeGeneratorSettings.cs`, `DependencyInjectionSettings.cs`, `NamingSettings.cs`, `TypeAccessibility.cs`, and `MultipleInterfaces.cs`. This overhaul would enhance maintainability of the settings configuration by segregating settings based on their respective areas of concern. Any future modifications or additions can now be applied to the respective class directly, thereby minimizing the potential impact radius of the change.

---
+++ src/Refitter.Core/Settings/NSwagCodeGeneratorSettings.cs
@@ -0,0 +1,169 @@
+﻿using System.Text.Json.Serialization;
+
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace Refitter.Core;
+
+/// <summary>
+/// CSharp code generator settings
+/// </summary>
+public class NSwagCodeGeneratorSettings
+{
+    /// <summary>
+    /// Gets or sets the .NET namespace of the generated types (default: GeneratedCode).
+    /// </summary>
+    public string Namespace { get; set; } = "GeneratedCode";
+
+    /// <summary>
+    /// Gets or sets a value indicating whether a required property must be defined in JSON
+    /// (sets Required.Always when the property is required) (default: true).
+    /// </summary>
+    public bool RequiredPropertiesMustBeDefined { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to generated data annotation attributes (default: true).
+    /// </summary>
+    public bool GenerateDataAnnotations { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the any type (default: "object").
+    /// </summary>
+    public string AnyType { get; set; } = "object";
+
+    /// <summary>
+    /// Gets or sets the date .NET type (default: 'DateTimeOffset').
+    /// </summary>
+    public string DateType { get; set; } = "System.DateTimeOffset";
+
+    /// <summary>
+    /// Gets or sets the date time .NET type (default: 'DateTimeOffset').
+    /// </summary>
+    public string DateTimeType { get; set; } = "System.DateTimeOffset";
+
+    /// <summary>
+    /// Gets or sets the time .NET type (default: 'TimeSpan').
+    /// </summary>
+    public string TimeType { get; set; } = "System.TimeSpan";
+
+    /// <summary>
+    /// Gets or sets the time span .NET type (default: 'TimeSpan').
+    /// </summary>
+    public string TimeSpanType { get; set; } = "System.TimeSpan";
+
+    /// <summary>
+    /// Gets or sets the generic array .NET type (default: 'ICollection').
+    /// </summary>
+    public string ArrayType { get; set; } = "System.Collections.Generic.ICollection";
+
+    /// <summary>
+    /// Gets or sets the generic dictionary .NET type (default: 'IDictionary').
+    /// </summary>
+    public string DictionaryType { get; set; } = "System.Collections.Generic.IDictionary";
+
+    /// <summary>
+    /// Gets or sets the generic array .NET type which is used for ArrayType instances (default: 'Collection').
+    /// </summary>
+    public string ArrayInstanceType { get; set; } = "System.Collections.ObjectModel.Collection";
+
+    /// <summary>
+    /// Gets or sets the generic dictionary .NET type which is used for DictionaryType instances (default: 'Dictionary').
+    /// </summary>
+    public string DictionaryInstanceType { get; set; } = "System.Collections.Generic.Dictionary";
+
+    /// <summary>
+    /// Gets or sets the generic array .NET type which is used as base class (default: 'Collection').
+    /// </summary>
+    public string ArrayBaseType { get; set; } = "System.Collections.ObjectModel.Collection";
+
+    /// <summary>
+    /// Gets or sets the generic dictionary .NET type which is used as base class (default: 'Dictionary').
+    /// </summary>
+    public string DictionaryBaseType { get; set; } = "System.Collections.Generic.Dictionary";
+
+    /// <summary>
+    /// Gets or sets the CSharp class style (default: 'Poco').
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public CSharpClassStyle ClassStyle { get; set; } = CSharpClassStyle.Poco;
+
+    /// <summary>
+    /// Gets or sets the CSharp JSON library to use (default: 'NewtonsoftJson', 'SystemTextJson' is experimental/not complete).
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public CSharpJsonLibrary JsonLibrary { get; set; } = CSharpJsonLibrary.NewtonsoftJson;
+
+    /// <summary>
+    /// Gets or sets the access modifier of generated classes and interfaces (default: 'public').
+    /// </summary>
+    public string TypeAccessModifier { get; set; } = "public";
+
+    /// <summary>
+    /// Gets the access modifier of property setters (default: '').
+    /// </summary>
+    public string PropertySetterAccessModifier { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the custom Json.NET converters (class names) which are registered for serialization and deserialization.
+    /// </summary>
+    public string[]? JsonConverters { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to remove the setter for non-nullable array properties (default: false).
+    /// </summary>
+    public bool GenerateImmutableArrayProperties { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to remove the setter for non-nullable dictionary properties (default: false).
+    /// </summary>
+    public bool GenerateImmutableDictionaryProperties { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to use preserve references handling (All) in the JSON serializer (default: false).
+    /// </summary>
+    public bool HandleReferences { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of a static method which is called to transform the JsonSerializerSettings (for Newtonsoft.Json) or the JsonSerializerOptions (for System.Text.Json) used in the generated ToJson()/FromJson() methods (default: null).
+    /// </summary>
+    public string? JsonSerializerSettingsTransformationMethod { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to render ToJson() and FromJson() methods (default: false).
+    /// </summary>
+    public bool GenerateJsonMethods { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether enums should be always generated as bit flags (default: false).
+    /// </summary>
+    public bool EnforceFlagEnums { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether named/referenced dictionaries should be inlined or generated as class with dictionary inheritance.
+    /// </summary>
+    public bool InlineNamedDictionaries { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether named/referenced tuples should be inlined or generated as class with tuple inheritance.
+    /// </summary>
+    public bool InlineNamedTuples { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether named/referenced arrays should be inlined or generated as class with array inheritance.
+    /// </summary>
+    public bool InlineNamedArrays { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether optional schema properties (not required) are generated as nullable properties (default: false).
+    /// </summary>
+    public bool GenerateOptionalPropertiesAsNullable { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to generate Nullable Reference Type annotations (default: false).
+    /// </summary>
+    public bool GenerateNullableReferenceTypes { get; set; }
+
+    /// <summary>
+    /// Generate C# 9.0 record types instead of record-like classes.
+    /// </summary>
+    public bool GenerateNativeRecords { get; set; }
+}
---
+++ src/Refitter.Core/Settings/TypeAccessibility.cs
@@ -0,0 +1,17 @@
+﻿namespace Refitter.Core;
+
+/// <summary>
+/// Specifies the accessibility of a type.
+/// </summary>
+public enum TypeAccessibility
+{
+    /// <summary>
+    /// Indicates that the type is accessible by any assembly that references it.
+    /// </summary>
+    Public,
+
+    /// <summary>
+    /// Indicates that the type is only accessible within its own assembly.
+    /// </summary>
+    Internal
+}
