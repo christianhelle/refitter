b1cc813d0fbd11f75cc6cf1c4560de0692813aed
Christian Helle <crh@delegate.dk>
2023-10-30 23:47:32 +0100

    Add DelegatingHandler test classes in Refitter.Tests
    Created two new test classes, namely `EmptyMessageHandler` and `AnotherEmptyMessageHandler`, for testing DelegatingHandler scenarios in the Refitter.Tests.AdditionalFiles.SingeInterface namespace. These classes are empty for now and will be populated with specific scenarios in future commits.

---
+++ src/Refitter.SourceGenerator.Tests/DelegatingHandlers.cs
@@ -0,0 +1,13 @@
+namespace Refitter.Tests.AdditionalFiles.SingeInterface
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class EmptyMessageHandler : DelegatingHandler
+    {
+    }
+
+    public class AnotherEmptyMessageHandler : DelegatingHandler 
+    {
+    }
+}
