c3022460a9ed87f5ab1e78f65a7f6709bc200f1b
Christian Helle <christian.helle@outlook.com>
2023-12-13 22:25:44 +0100

    Add sample Maui app

---
+++ test/MauiExample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Petstore;
+
+namespace MauiExample;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+	ISwaggerPetstore petstore;
+
+	public MainPage(ISwaggerPetstore petstore)
+	{
+		InitializeComponent();
+		this.petstore = petstore;
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		try
+		{
+			var response = await petstore.GetPetById(count);
+			if (response.StatusCode != System.Net.HttpStatusCode.OK)
+				CounterBtn.Text = $"Error: {response.StatusCode}";
+			else
+				CounterBtn.Text = response.Content.Name;
+		}
+		catch (Refit.ApiException ex)
+		{
+			CounterBtn.Text = "Refit call failed: " + ex.Message;			
+		}
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
---
+++ test/MauiExample/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Petstore;
+
+namespace MauiExample;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.ConfigureRefitClients();
+
+		return builder.Build();
+	}
+}
