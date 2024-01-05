69f7ff6f1901d8fdafbb4e8b9692e4c47fc7a01b
Christian Helle <christian.helle@outlook.com>
2024-01-05 15:18:42 +0100

    Refactor OpenApiDocumentFactory to populate missing required fields

--- src/Refitter.Core/OpenApiDocumentFactory.cs
+++ src/Refitter.Core/OpenApiDocumentFactory.cs
@@ -1,24 +1,29 @@
 ﻿using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi.Readers;
 
 using NSwag;
 
+using OpenApiDocument = NSwag.OpenApiDocument;
+
 namespace Refitter.Core;
 
 /// <summary>
-/// Creates an <see cref="OpenApiDocument"/> from a specified path or URL.
+/// Creates an <see cref="NSwag.OpenApiDocument"/> from a specified path or URL.
 /// </summary>
 public static class OpenApiDocumentFactory
 {
     /// <summary>
-    /// Creates a new instance of the <see cref="OpenApiDocument"/> class asynchronously.
+    /// Creates a new instance of the <see cref="NSwag.OpenApiDocument"/> class asynchronously.
     /// </summary>
     /// <param name="openApiPath">The path or URL to the OpenAPI specification.</param>
-    /// <returns>A new instance of the <see cref="OpenApiDocument"/> class.</returns>
+    /// <returns>A new instance of the <see cref="NSwag.OpenApiDocument"/> class.</returns>
     public static async Task<OpenApiDocument> CreateAsync(string openApiPath)
     {
         var readResult = await OpenApiMultiFileReader.Read(openApiPath);
         var specificationVersion = readResult.OpenApiDiagnostic.SpecificationVersion;
+
+        PopulateMissingRequiredFields(openApiPath, readResult);
 
         if (IsYaml(openApiPath))
         {
@@ -28,6 +33,26 @@ public static class OpenApiDocumentFactory
 
         var json = readResult.OpenApiDocument.SerializeAsJson(specificationVersion);
         return await OpenApiDocument.FromJsonAsync(json);
+    }
+
+    private static void PopulateMissingRequiredFields(
+        string openApiPath,
+        ReadResult readResult)
+    {
+        var document = readResult.OpenApiDocument;
+        if (document.Info is null)
+        {
+            document.Info = new Microsoft.OpenApi.Models.OpenApiInfo
+            {
+                Title = Path.GetFileNameWithoutExtension(openApiPath),
+                Version = readResult.OpenApiDiagnostic.SpecificationVersion.GetDisplayName()
+            };
+        }
+        else
+        {
+            document.Info.Title ??= Path.GetFileNameWithoutExtension(openApiPath);
+            document.Info.Version ??= readResult.OpenApiDiagnostic.SpecificationVersion.GetDisplayName();
+        }
     }
 
     /// <summary>
