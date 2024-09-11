e36c9aba1de95ac3ed508d84bf38625e22fd67f1
Christian Helle <christian.helle@outlook.com>
2024-09-11 10:02:38 +0200

    Update CLI option description for --use-dynamic-querystring-parameters

--- src/Refitter/Settings.cs
+++ src/Refitter/Settings.cs
@@ -204,7 +204,7 @@ public sealed class Settings : CommandSettings
     public bool UseApizr { get; set; }
 
     [Description("""
-                 Set to <c>true</c> to wrap multiple query parameters into a single complex one. Default is <c>false</c> (no wrapping).
+                 Enable wrapping multiple query parameters into a single complex one. Default is no wrapping.
                  See https://github.com/reactiveui/refit?tab=readme-ov-file#dynamic-querystring-parameters for more information.
                  """)]
     [CommandOption("--use-dynamic-querystring-parameters")]
