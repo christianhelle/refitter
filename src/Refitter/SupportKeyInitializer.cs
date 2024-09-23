e55c87cc3e47c0194e08660859c0b4339b47f704
Christian Helle <christian.helle@outlook.com>
2024-09-23 13:59:31 +0200

    Log telemetry to both Exceptionless and App Insights

---
+++ src/Refitter/SupportKeyInitializer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Refitter;
+
+public sealed class SupportKeyInitializer : ITelemetryInitializer
+{
+    public void Initialize(ITelemetry telemetry)
+    {
+        if (telemetry is ISupportProperties supportProperties)
+            supportProperties.Properties["support-key"] = SupportInformation.GetSupportKey();
+    }
+}
