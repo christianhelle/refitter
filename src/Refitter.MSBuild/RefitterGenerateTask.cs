1f72f7651a375a9db6632fc6a92977270a13832a
Christian Helle <christian.helle@outlook.com>
2024-12-07 14:40:39 +0100

    Create MSBuild project with custom task

---
+++ src/Refitter.MSBuild/RefitterGenerateTask.cs
@@ -0,0 +1,103 @@
+using System.Diagnostics;
+using MSBuildTask = Microsoft.Build.Utilities.Task;
+
+namespace Refitter.MSBuild;
+
+public class RefitterGenerateTask : MSBuildTask
+{
+    public string ProjectFileDirectory { get; set; }
+
+    public override bool Execute()
+    {
+        TryLogCommandLine($"Starting {nameof(RefitterGenerateTask)}");
+        TryLogCommandLine($"Looking for .refitter files under {ProjectFileDirectory}");
+
+        var files = Directory.GetFiles(
+            ProjectFileDirectory,
+            "*.refitter",
+            SearchOption.AllDirectories);
+
+        TryLogCommandLine($"Found {files.Length} .refitter files...");        
+
+        foreach (var file in files)
+        {
+            TryLogCommandLine($"Processing {file}");
+            TryExecuteRefitter(file);
+        }
+
+        return true;
+    }
+
+    private void TryExecuteRefitter(string file)
+    {
+        try
+        {
+            StartProcess(file);
+        }
+        catch (Exception e)
+        {
+            TryLogErrorFromException(e);
+        }
+    }
+
+    private void StartProcess(string file)
+    {
+        using var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "refitter",
+                Arguments = $"--settings-file {file}",
+                WorkingDirectory = Path.GetDirectoryName(file)!,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+
+        process.ErrorDataReceived += (_, args) => TryLogError(args.Data);
+        process.OutputDataReceived += (_, args) => TryLogCommandLine(args.Data);
+        process.Start();
+        process.BeginErrorReadLine();
+        process.BeginOutputReadLine();
+        process.WaitForExit();
+    }
+
+    private void TryLogErrorFromException(Exception e)
+    {
+        try
+        {
+            Log.LogErrorFromException(e);
+        }
+        catch
+        {
+            // Ignore
+        }
+    }
+
+    private void TryLogCommandLine(string text)
+    {
+        try
+        {
+            Log.LogCommandLine(text);
+        }
+        catch
+        {
+            // ignore
+        }
+    }
+
+    private void TryLogError(string text)
+    {
+        try
+        {
+            Log.LogError(text);
+        }
+        catch
+        {
+            // ignore
+        }
+    }
+}
