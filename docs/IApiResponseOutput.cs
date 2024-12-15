c712a19de280ba252d06ed74d3b433988aca9373
Christian Helle <christian.helle@outlook.com>
2024-12-15 22:33:45 +0100

    Output code examples

---
+++ docs/IApiResponseOutput.cs
@@ -0,0 +1,397 @@
+using Refit;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Your.Namespace.Of.Choice.GeneratedCode
+{
+    [System.CodeDom.Compiler.GeneratedCode("Refitter", "1.0.0.0")]
+    public partial interface ISwaggerPetstore
+    {
+        /// <summary>Update an existing pet</summary>
+        /// <remarks>Update an existing pet by Id</remarks>
+        /// <param name="body">Update an existent pet in the store</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>Successful operation</description>
+        /// </item>
+        /// <item>
+        /// <term>400</term>
+        /// <description>Invalid ID supplied</description>
+        /// </item>
+        /// <item>
+        /// <term>404</term>
+        /// <description>Pet not found</description>
+        /// </item>
+        /// <item>
+        /// <term>405</term>
+        /// <description>Validation exception</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/xml, application/json")]
+        [Put("/pet")]
+        Task<IApiResponse<Pet>> UpdatePet([Body] Pet body);
+
+        /// <summary>Add a new pet to the store</summary>
+        /// <remarks>Add a new pet to the store</remarks>
+        /// <param name="body">Create a new pet in the store</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>Successful operation</description>
+        /// </item>
+        /// <item>
+        /// <term>405</term>
+        /// <description>Invalid input</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/xml, application/json")]
+        [Post("/pet")]
+        Task<IApiResponse<Pet>> AddPet([Body] Pet body);
+
+        /// <summary>Finds Pets by status</summary>
+        /// <remarks>Multiple status values can be provided with comma separated strings</remarks>
+        /// <param name="status">Status values that need to be considered for filter</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>successful operation</description>
+        /// </item>
+        /// <item>
+        /// <term>400</term>
+        /// <description>Invalid status value</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/json")]
+        [Get("/pet/findByStatus")]
+        Task<IApiResponse<ICollection<Pet>>> FindPetsByStatus([Query] Status? status);
+
+        /// <summary>Finds Pets by tags</summary>
+        /// <remarks>Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing.</remarks>
+        /// <param name="tags">Tags to filter by</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>successful operation</description>
+        /// </item>
+        /// <item>
+        /// <term>400</term>
+        /// <description>Invalid tag value</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/json")]
+        [Get("/pet/findByTags")]
+        Task<IApiResponse<ICollection<Pet>>> FindPetsByTags([Query(CollectionFormat.Multi)] IEnumerable<string> tags);
+
+        /// <summary>Find pet by ID</summary>
+        /// <remarks>Returns a single pet</remarks>
+        /// <param name="petId">ID of pet to return</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>successful operation</description>
+        /// </item>
+        /// <item>
+        /// <term>400</term>
+        /// <description>Invalid ID supplied</description>
+        /// </item>
+        /// <item>
+        /// <term>404</term>
+        /// <description>Pet not found</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/xml, application/json")]
+        [Get("/pet/{petId}")]
+        Task<IApiResponse<Pet>> GetPetById(long petId);
+
+        /// <summary>Updates a pet in the store with form data</summary>
+        /// <param name="petId">ID of pet that needs to be updated</param>
+        /// <param name="name">Name of pet that needs to be updated</param>
+        /// <param name="status">Status of pet that needs to be updated</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>405</term>
+        /// <description>Invalid input</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Post("/pet/{petId}")]
+        Task<IApiResponse> UpdatePetWithForm(long petId, [Query] string name, [Query] string status);
+
+        /// <summary>Deletes a pet</summary>
+        /// <param name="petId">Pet id to delete</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>400</term>
+        /// <description>Invalid pet value</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Delete("/pet/{petId}")]
+        Task<IApiResponse> DeletePet(long petId, [Header("api_key")] string api_key);
+
+        /// <summary>uploads an image</summary>
+        /// <param name="petId">ID of pet to update</param>
+        /// <param name="additionalMetadata">Additional Metadata</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>successful operation</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/json")]
+        [Post("/pet/{petId}/uploadImage")]
+        Task<IApiResponse<ApiResponse>> UploadFile(long petId, [Query] string additionalMetadata,  StreamPart body);
+
+        /// <summary>Returns pet inventories by status</summary>
+        /// <remarks>Returns a map of status codes to quantities</remarks>
+        /// <returns>successful operation</returns>
+        /// <exception cref="ApiException">Thrown when the request returns a non-success status code.</exception>
+        [Headers("Accept: application/json")]
+        [Get("/store/inventory")]
+        Task<IApiResponse<IDictionary<string, int>>> GetInventory();
+
+        /// <summary>Place an order for a pet</summary>
+        /// <remarks>Place a new order in the store</remarks>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>successful operation</description>
+        /// </item>
+        /// <item>
+        /// <term>405</term>
+        /// <description>Invalid input</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/json")]
+        [Post("/store/order")]
+        Task<IApiResponse<Order>> PlaceOrder([Body] Order body);
+
+        /// <summary>Find purchase order by ID</summary>
+        /// <remarks>For valid response try integer IDs with value <= 5 or > 10. Other values will generated exceptions</remarks>
+        /// <param name="orderId">ID of order that needs to be fetched</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>successful operation</description>
+        /// </item>
+        /// <item>
+        /// <term>400</term>
+        /// <description>Invalid ID supplied</description>
+        /// </item>
+        /// <item>
+        /// <term>404</term>
+        /// <description>Order not found</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/json")]
+        [Get("/store/order/{orderId}")]
+        Task<IApiResponse<Order>> GetOrderById(long orderId);
+
+        /// <summary>Delete purchase order by ID</summary>
+        /// <remarks>For valid response try integer IDs with value < 1000. Anything above 1000 or nonintegers will generate API errors</remarks>
+        /// <param name="orderId">ID of the order that needs to be deleted</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>400</term>
+        /// <description>Invalid ID supplied</description>
+        /// </item>
+        /// <item>
+        /// <term>404</term>
+        /// <description>Order not found</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Delete("/store/order/{orderId}")]
+        Task<IApiResponse> DeleteOrder(long orderId);
+
+        /// <summary>Create user</summary>
+        /// <remarks>This can only be done by the logged in user.</remarks>
+        /// <param name="body">Created user object</param>
+        /// <returns>A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result.</returns>
+        [Headers("Accept: application/json, application/xml")]
+        [Post("/user")]
+        Task<IApiResponse> CreateUser([Body] User body);
+
+        /// <summary>Creates list of users with given input array</summary>
+        /// <remarks>Creates list of users with given input array</remarks>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>Successful operation</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/xml, application/json")]
+        [Post("/user/createWithList")]
+        Task<IApiResponse<User>> CreateUsersWithListInput([Body] IEnumerable<User> body);
+
+        /// <summary>Logs user into the system</summary>
+        /// <param name="username">The user name for login</param>
+        /// <param name="password">The password for login in clear text</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>successful operation</description>
+        /// </item>
+        /// <item>
+        /// <term>400</term>
+        /// <description>Invalid username/password supplied</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/json")]
+        [Get("/user/login")]
+        Task<IApiResponse<string>> LoginUser([Query] string username, [Query] string password);
+
+        /// <summary>Logs out current logged in user session</summary>
+        /// <returns>A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result.</returns>
+        [Get("/user/logout")]
+        Task<IApiResponse> LogoutUser();
+
+        /// <summary>Get user by user name</summary>
+        /// <param name="username">The name that needs to be fetched. Use user1 for testing.</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>200</term>
+        /// <description>successful operation</description>
+        /// </item>
+        /// <item>
+        /// <term>400</term>
+        /// <description>Invalid username supplied</description>
+        /// </item>
+        /// <item>
+        /// <term>404</term>
+        /// <description>User not found</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Headers("Accept: application/json")]
+        [Get("/user/{username}")]
+        Task<IApiResponse<User>> GetUserByName(string username);
+
+        /// <summary>Update user</summary>
+        /// <remarks>This can only be done by the logged in user.</remarks>
+        /// <param name="username">name that need to be deleted</param>
+        /// <param name="body">Update an existent user in the store</param>
+        /// <returns>A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result.</returns>
+        [Put("/user/{username}")]
+        Task<IApiResponse> UpdateUser(string username, [Body] User body);
+
+        /// <summary>Delete user</summary>
+        /// <remarks>This can only be done by the logged in user.</remarks>
+        /// <param name="username">The name that needs to be deleted</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
+        /// <list type="table">
+        /// <listheader>
+        /// <term>Status</term>
+        /// <description>Description</description>
+        /// </listheader>
+        /// <item>
+        /// <term>400</term>
+        /// <description>Invalid username supplied</description>
+        /// </item>
+        /// <item>
+        /// <term>404</term>
+        /// <description>User not found</description>
+        /// </item>
+        /// </list>
+        /// </returns>
+        [Delete("/user/{username}")]
+        Task<IApiResponse> DeleteUser(string username);
+    }
+}
