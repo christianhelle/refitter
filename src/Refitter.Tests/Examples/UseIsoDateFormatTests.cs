8699e2ea788994caff02e494ffa4694aea9b7875
Christian Helle <christian.helle@outlook.com>
2025-01-28 13:40:27 +0100

    Fix date formatting for date-time types

--- src/Refitter.Tests/Examples/UseIsoDateFormatTests.cs
+++ src/Refitter.Tests/Examples/UseIsoDateFormatTests.cs
@@ -8,66 +8,54 @@ namespace Refitter.Tests.Examples;
 public class UseIsoDateFormatTests
 {
     private const string OpenApiSpec = @"
-{
-  ""swagger"": ""2.0"",
-  ""info"": {
-    ""title"": ""XX"",
-    ""version"": ""0.0.0""
-  },
-  ""host"": ""x.io"",
-  ""basePath"": ""/"",
-  ""schemes"": [
-    ""https""
-  ],
-  ""paths"": {
-    ""/t/dummy/{employee_id}"": {
-      ""get"": {
-        ""summary"": ""X"",
-        ""description"": ""X"",
-        ""operationId"": ""dummy"",
-        ""parameters"": [
-          {
-            ""name"": ""employee_id"",
-            ""in"": ""path"",
-            ""description"": ""the specific employee"",
-            ""required"": true,
-            ""format"": ""int64"",
-            ""type"": ""integer""
-          },
-          {
-            ""name"": ""valid_from"",
-            ""in"": ""query"",
-            ""description"": ""the start of the period"",
-            ""required"": true,
-            ""format"": ""date"",
-            ""type"": ""string""
-          },
-          {
-            ""name"": ""valid_to"",
-            ""in"": ""query"",
-            ""description"": ""the end of the period"",
-            ""required"": true,
-            ""format"": ""date"",
-            ""type"": ""string""
-          },
-          {
-            ""name"": ""test_time"",
-            ""in"": ""query"",
-            ""description"": ""test parameter"",
-            ""required"": true,
-            ""format"": ""time"",
-            ""type"": ""string""
-          }
-        ],
-        ""responses"": {
-          ""200"": {
-            ""description"": ""No response was specified""
-          }
-        }
-      }
-    },
-  }
-}
+swagger: '2.0'
+info:
+  title: XX
+  version: 0.0.0
+host: x.io
+basePath: /
+schemes:
+  - https
+paths:
+  '/t/dummy/{employee_id}':
+    get:
+      summary: X
+      description: X
+      operationId: dummy
+      parameters:
+        - name: employee_id
+          in: path
+          description: the specific employee
+          required: true
+          format: int64
+          type: integer
+        - name: valid_from
+          in: query
+          description: the start of the period
+          required: true
+          format: date
+          type: string
+        - name: valid_to
+          in: query
+          description: the end of the period
+          required: true
+          format: date
+          type: string
+        - name: test_time
+          in: query
+          description: test parameter
+          required: true
+          format: time
+          type: string
+        - name: test_datetime
+          in: query
+          description: test parameter
+          required: true
+          format: date-time
+          type: string
+      responses:
+        '200':
+          description: No response was specified
 ";
 
     [Fact]
@@ -81,7 +69,16 @@ public class UseIsoDateFormatTests
     public async Task GeneratedCode_Contains_Date_Format_String()
     {
         string generateCode = await GenerateCode();
-        generateCode.Should().Contain(@"[Query(Format = ""yyyy-MM-dd"")] ");
+        generateCode.Should().Contain(@"[Query(Format = ""yyyy-MM-dd"")] System.DateTimeOffset valid_from");
+        generateCode.Should().Contain(@"[Query(Format = ""yyyy-MM-dd"")] System.DateTimeOffset valid_to");
+    }
+
+    [Fact]
+    public async Task GeneratedCode_NotContains_DateTime_Format_String()
+    {
+        string generateCode = await GenerateCode();
+        generateCode.Should().Contain(@"[Query] System.DateTimeOffset test_datetime");
+        generateCode.Should().NotContain(@"[Query(Format = ""yyyy-MM-dd"")] System.DateTimeOffset time_datetime");
     }
 
     [Fact]
@@ -104,11 +101,7 @@ public class UseIsoDateFormatTests
     private static async Task<string> GenerateCode()
     {
         var swaggerFile = await CreateSwaggerFile(OpenApiSpec);
-        var settings = new RefitGeneratorSettings
-        {
-            OpenApiPath = swaggerFile,
-            UseIsoDateFormat = true
-        };
+        var settings = new RefitGeneratorSettings { OpenApiPath = swaggerFile, UseIsoDateFormat = true };
 
         var sut = await RefitGenerator.CreateAsync(settings);
         var generateCode = sut.Generate();
@@ -117,7 +110,7 @@ public class UseIsoDateFormatTests
 
     private static async Task<string> CreateSwaggerFile(string contents)
     {
-        var filename = $"{Guid.NewGuid()}.json";
+        var filename = $"{Guid.NewGuid()}.yaml";
         var folder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
         Directory.CreateDirectory(folder);
         var swaggerFile = Path.Combine(folder, filename);
