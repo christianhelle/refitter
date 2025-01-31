01184ebbe9ce9837705bd7559823336501554717
Christian Helle <christian.helle@outlook.com>
2025-01-31 23:46:29 +0100

    Add description annotations for MultipleInterfaces and TransientErrorHandler enums

--- src/Refitter.Core/Settings/MultipleInterfaces.cs
+++ src/Refitter.Core/Settings/MultipleInterfaces.cs
@@ -1,23 +1,34 @@
-﻿namespace Refitter.Core;
+using System.ComponentModel;
+
+namespace Refitter.Core;
 
 /// <summary>
 /// Enum representing the different options for generating multiple Refit interfaces.
 /// </summary>
+[Description("Enum representing the different options for generating multiple Refit interfaces.")]
 public enum MultipleInterfaces
 {
     /// <summary>
     /// Do not generate multiple interfaces
     /// </summary>
+    [Description("Do not generate multiple interfaces")]
     Unset,
 
     /// <summary>
-    /// Generate a Refit interface for each endpoint with a single Execute() method. 
+    /// Generate a Refit interface for each endpoint with a single Execute() method.
     /// The method name can be customized using the <see cref="RefitGeneratorSettings.OperationNameTemplate"/> setting.
     /// </summary>
+    [Description(
+        """
+            Generate a Refit interface for each endpoint with a single Execute() method.
+            The method name can be customized using the OperationNameTemplate setting.
+            """
+    )]
     ByEndpoint,
 
     /// <summary>
     /// Generate a Refit interface for each tag
     /// </summary>
+    [Description("Generate a Refit interface for each tag")]
     ByTag
 }
