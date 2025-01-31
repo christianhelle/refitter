e4353d38a2901c57a83760d4770581ae5c9f7f20
Christian Helle <christian.helle@outlook.com>
2025-02-01 00:05:17 +0100

    Add description annotations in ApizrSettings

--- src/Refitter.Core/Settings/ApizrSettings.cs
+++ src/Refitter.Core/Settings/ApizrSettings.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Text.Json.Serialization;
 
 namespace Refitter.Core
@@ -6,18 +7,31 @@ namespace Refitter.Core
     /// Describing how Apizr should be configured.
     /// Here are only the common configurations.
     /// </summary>
+    [Description("Describing how Apizr should be configured. Here are only the common configurations.")]
     public class ApizrSettings
     {
         /// <summary>
         /// Set it to true to include an Apizr Request Options parameter into all api methods
         /// and get all the Apizr goodness (default: true)
         /// </summary>
+        [Description(
+            """
+            Set it to true to include an Apizr Request Options parameter into all api methods
+            and get all the Apizr goodness (default: true)
+            """
+        )]
         public bool WithRequestOptions { get; set; } = true;
 
         /// <summary>
         /// Set it to true to generate an Apizr registration helper ready to use (default: false).
         /// Please note that it will generate an extended or static helper depending on DependencyInjectionSettings property value.
         /// </summary>
+        [Description(
+            """
+            Set it to true to generate an Apizr registration helper ready to use (default: false).
+            Please note that it will generate an extended or static helper depending on DependencyInjectionSettings property value.
+            """
+        )]
         public bool WithRegistrationHelper { get; set; } = false;
 
         /// <summary>
@@ -29,6 +43,17 @@ namespace Refitter.Core
         /// - InMemory (Microsoft.Extensions.Caching.Memory)
         /// - Distributed (Microsoft.Extensions.Caching.Distributed)
         /// </summary>
+        [Description(
+            """
+            Library to use for cache handling (default: None)
+            Options:
+            - None
+            - Akavache
+            - MonkeyCache
+            - InMemory (Microsoft.Extensions.Caching.Memory)
+            - Distributed (Microsoft.Extensions.Caching.Distributed)
+            """
+        )]
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public CacheProviderType WithCacheProvider { get; set; }
 
@@ -39,22 +64,34 @@ namespace Refitter.Core
         /// - AutoMapper
         /// - Mapster
         /// </summary>
+        [Description(
+            """
+            Library to use for data mapping handling (default: None)
+            Options:
+            - None
+            - AutoMapper
+            - Mapster
+            """
+        )]
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public MappingProviderType WithMappingProvider { get; set; }
 
         /// <summary>
         /// Set it to true to handle request with priority (default: false)
         /// </summary>
+        [Description("Set it to true to handle request with priority (default: false)")]
         public bool WithPriority { get; set; } = false;
 
         /// <summary>
         /// Set it to true to handle request with MediatR (default: false)
         /// </summary>
+        [Description("Set it to true to handle request with MediatR (default: false)")]
         public bool WithMediation { get; set; } = false;
 
         /// <summary>
         /// Set it to true to manage file transfers (default: false)
         /// </summary>
+        [Description("Set it to true to manage file transfers (default: false)")]
         public bool WithFileTransfer { get; set; } = false;
     }
 }
