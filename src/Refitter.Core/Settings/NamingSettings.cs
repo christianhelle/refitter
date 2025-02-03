efd51ab0d4181e409d8222d8aeda74063dba2804
Christian Helle <christian.helle@outlook.com>
2025-02-03 20:12:52 +0100

    Add description annotations to NamingSettings

--- src/Refitter.Core/Settings/NamingSettings.cs
+++ src/Refitter.Core/Settings/NamingSettings.cs
@@ -1,10 +1,12 @@
-﻿using System.Diagnostics.CodeAnalysis;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Refitter.Core;
 
 /// <summary>
 /// Configurable settings for naming in the client API
 /// </summary>
+[Description("Configurable settings for naming the client API interface")]
 [ExcludeFromCodeCoverage]
 public class NamingSettings
 {
@@ -13,10 +15,17 @@ public class NamingSettings
     /// <summary>
     /// Gets or sets a value indicating whether the OpenApi title should be used. Default is true.
     /// </summary>
+    [Description(
+        """
+            Set to true to use the title defined in the OpenAPI document for the interface name.
+            Default is true
+            """
+    )]
     public bool UseOpenApiTitle { get; set; } = true;
 
     /// <summary>
     /// Gets or sets the name of the Interface. Default is "ApiClient".
     /// </summary>
+    [Description("The name of the interface if UseOpenApiTitle is set to false. Default is IApiClient")]
     public string InterfaceName { get; set; } = DefaultInterfaceName;
 }
