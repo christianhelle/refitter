48775fb06e38492dd1b0bf67a07b2c85505fb588
Christian Helle <christian.helle@outlook.com>
2025-04-28 09:50:14 +0200

    Add CollectionFormat enum for configuring array query parameter format

---
+++ src/Refitter.Core/Settings/CollectionFormat.cs
@@ -0,0 +1,37 @@
+namespace Refitter.Core;
+
+/// <summary>
+/// Collection format defined in https://swagger.io/docs/specification/2-0/describing-parameters/
+/// </summary>
+public enum CollectionFormat
+{
+    /// <summary>
+    /// Values formatted with UrlParameterFormatter or FormUrlEncodedParameterFormatter
+    /// </summary>
+    RefitParameterFormatter,
+
+    /// <summary>
+    /// Comma-separated values
+    /// </summary>
+    Csv,
+
+    /// <summary>
+    /// Space-separated values
+    /// </summary>
+    Ssv,
+
+    /// <summary>
+    /// Tab-separated values
+    /// </summary>
+    Tsv,
+
+    /// <summary>
+    /// Pipe-separated values
+    /// </summary>
+    Pipes,
+
+    /// <summary>
+    /// Multiple parameter instances
+    /// </summary>
+    Multi
+}
