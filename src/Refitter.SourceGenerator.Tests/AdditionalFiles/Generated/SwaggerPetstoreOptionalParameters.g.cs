ab3ffd54d63c76809fd23859503aebf314d1c0d4
Christian Helle <christian.helle@outlook.com>
2025-07-04 14:12:08 +0200

    Re-generate source generator test output

--- src/Refitter.SourceGenerator.Tests/AdditionalFiles/Generated/SwaggerPetstoreOptionalParameters.g.cs
+++ src/Refitter.SourceGenerator.Tests/AdditionalFiles/Generated/SwaggerPetstoreOptionalParameters.g.cs
@@ -12,6 +12,16 @@ using System.Threading.Tasks;
 
 namespace Refitter.Tests.AdditionalFiles.OptionalParameters
 {
+    /// <summary>
+    /// This is a sample Pet Store Server based on the OpenAPI 3.0 specification.  You can find out more about
+    /// Swagger at [http://swagger.io](http://swagger.io). In the third iteration of the pet store, we've switched to the design first approach!
+    /// You can now help us improve the API whether it's by making changes to the definition itself or to the code.
+    /// That way, with time, we can improve the API in general, and expose some of the new features in OAS3.
+    /// 
+    /// Some useful links:
+    /// - [The Pet Store repository](https://github.com/swagger-api/swagger-petstore)
+    /// - [The source API definition for the Pet Store](https://github.com/swagger-api/swagger-petstore/blob/master/src/main/resources/openapi.yaml)
+    /// </summary>
     [System.CodeDom.Compiler.GeneratedCode("Refitter", "1.0.0.0")]
     public partial interface ISwaggerPetstoreWithOptionalParameters
     {
