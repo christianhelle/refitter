de25ec60893e583e7093c9904c1ade016105bae2
Christian Helle <christian.helle@outlook.com>
2025-09-11 17:45:13 +0200

    Fix MSBuild test project build script

--- test/MSBuild/build.ps1
+++ test/MSBuild/build.ps1
@@ -9,9 +9,9 @@ dotnet clean -c release ../../src/Refitter.sln
 dotnet build -c release ../../src/Refitter/Refitter.csproj
 dotnet build -c release ../../src/Refitter.MSBuild/Refitter.MSBuild.csproj
 dotnet pack -c release ../../src/Refitter.MSBuild/Refitter.MSBuild.csproj -o .
-dotnet nuget add .\Refitter.MSBuild.1.0.0.nupkg -source .
+dotnet add package .\Refitter.MSBuild.1.0.0.nupkg --source .
 dotnet restore
-dotnet add package Refitter.MSBuild -s .
+dotnet add package Refitter.MSBuild --source .
 dotnet run -v d -filelogger
 dotnet remove package Refitter.MSBuild
 Remove-Item Refitter.MSBuild.*.nupkg -Force
