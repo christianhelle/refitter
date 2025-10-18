1fe339a5eee05eca2612d227c90e4b00adc4c7b3
Christian Helle <christian.helle@outlook.com>
2025-10-18 23:36:40 +0200

    Refactor Swagger file creation methods to use a shared helper for extension handling

--- src/Refitter.Tests/TestUtilities/SwaggerFileHelper.cs
+++ src/Refitter.Tests/TestUtilities/SwaggerFileHelper.cs
@@ -13,24 +13,20 @@ public static class SwaggerFileHelper
     /// </summary>
     /// <param name="contents">The OpenAPI specification content</param>
     /// <returns>The path to the created temporary file</returns>
-    public static async Task<string> CreateSwaggerFile(string contents)
-    {
-        var filename = $"{Guid.NewGuid()}.yml";
-        var folder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-        Directory.CreateDirectory(folder);
-        var swaggerFile = Path.Combine(folder, filename);
-        await File.WriteAllTextAsync(swaggerFile, contents);
-        return swaggerFile;
-    }
+    public static Task<string> CreateSwaggerFile(string contents) =>
+        CreateSwaggerFileWithExtension(contents, ".yml");
 
     /// <summary>
     /// Creates a temporary JSON file with the specified OpenAPI content
     /// </summary>
     /// <param name="contents">The OpenAPI specification content</param>
     /// <returns>The path to the created temporary file</returns>
-    public static async Task<string> CreateSwaggerJsonFile(string contents)
+    public static Task<string> CreateSwaggerJsonFile(string contents) =>
+        CreateSwaggerFileWithExtension(contents, ".json");
+
+    private static async Task<string> CreateSwaggerFileWithExtension(string contents, string extension)
     {
-        var filename = $"{Guid.NewGuid()}.json";
+        var filename = $"{Guid.NewGuid()}{extension}";
         var folder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
         Directory.CreateDirectory(folder);
         var swaggerFile = Path.Combine(folder, filename);
