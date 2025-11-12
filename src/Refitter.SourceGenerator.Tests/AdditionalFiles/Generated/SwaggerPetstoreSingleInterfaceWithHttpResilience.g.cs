41fa32f6f21813429bed66f4e3bb39d31f91c782
Christian Helle <christian.helle@outlook.com>
2025-11-12 10:21:16 +0100

    Re-generated source generator test output

--- src/Refitter.SourceGenerator.Tests/AdditionalFiles/Generated/SwaggerPetstoreSingleInterfaceWithHttpResilience.g.cs
+++ src/Refitter.SourceGenerator.Tests/AdditionalFiles/Generated/SwaggerPetstoreSingleInterfaceWithHttpResilience.g.cs
@@ -749,7 +749,7 @@ namespace Refitter.Tests.AdditionalFiles.SingeInterfaceWithHttpResilience
         /// Configures the Refit clients for dependency injection.
         /// </summary>
         /// <param name="services">The service collection to configure.</param>
-        /// <param name="builder">Optional action to configure the HTTP client builder.</param>
+/// <param name="builder">Optional action to configure the HTTP client builder.</param>
         /// <param name="settings">Optional Refit settings to customize serialization and other behaviors.</param>
         /// <returns>The configured service collection.</returns>
         public static IServiceCollection ConfigureRefitClients(
--- src/Refitter.SourceGenerator.Tests/AdditionalFiles/Generated/SwaggerPetstoreSingleInterfaceWithPolly.g.cs
+++ src/Refitter.SourceGenerator.Tests/AdditionalFiles/Generated/SwaggerPetstoreSingleInterfaceWithPolly.g.cs
@@ -12,363 +12,77 @@ using System.Threading.Tasks;
 
 namespace Refitter.Tests.AdditionalFiles.SingeInterface
 {
-    /// <summary>Swagger Petstore - OpenAPI 3.0</summary>
     [System.CodeDom.Compiler.GeneratedCode("Refitter", "1.0.0.0")]
     public partial interface ISwaggerPetstoreInterface
     {
-        /// <summary>Update an existing pet</summary>
-        /// <remarks>Update an existing pet by Id</remarks>
-        /// <param name="body">Update an existent pet in the store</param>
-        /// <returns>Successful operation</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>400</term>
-        /// <description>Invalid ID supplied</description>
-        /// </item>
-        /// <item>
-        /// <term>404</term>
-        /// <description>Pet not found</description>
-        /// </item>
-        /// <item>
-        /// <term>405</term>
-        /// <description>Validation exception</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Headers("Accept: application/xml, application/json", "Content-Type: application/json")]
         [Put("/pet")]
         Task<Pet> UpdatePet([Body] Pet body);
 
-        /// <summary>Add a new pet to the store</summary>
-        /// <remarks>Add a new pet to the store</remarks>
-        /// <param name="body">Create a new pet in the store</param>
-        /// <returns>Successful operation</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>405</term>
-        /// <description>Invalid input</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Headers("Accept: application/xml, application/json", "Content-Type: application/json")]
         [Post("/pet")]
         Task<Pet> AddPet([Body] Pet body);
 
-        /// <summary>Finds Pets by status</summary>
-        /// <remarks>Multiple status values can be provided with comma separated strings</remarks>
-        /// <param name="status">Status values that need to be considered for filter</param>
-        /// <returns>successful operation</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>400</term>
-        /// <description>Invalid status value</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Headers("Accept: application/json")]
         [Get("/pet/findByStatus")]
         Task<ICollection<Pet>> FindPetsByStatus([Query] Status? status);
 
-        /// <summary>Finds Pets by tags</summary>
-        /// <remarks>Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing.</remarks>
-        /// <param name="tags">Tags to filter by</param>
-        /// <returns>successful operation</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>400</term>
-        /// <description>Invalid tag value</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Headers("Accept: application/json")]
         [Get("/pet/findByTags")]
         Task<ICollection<Pet>> FindPetsByTags([Query(CollectionFormat.Multi)] IEnumerable<string> tags);
 
-        /// <summary>Find pet by ID</summary>
-        /// <remarks>Returns a single pet</remarks>
-        /// <param name="petId">ID of pet to return</param>
-        /// <returns>successful operation</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>400</term>
-        /// <description>Invalid ID supplied</description>
-        /// </item>
-        /// <item>
-        /// <term>404</term>
-        /// <description>Pet not found</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Headers("Accept: application/xml, application/json")]
         [Get("/pet/{petId}")]
         Task<Pet> GetPetById(long petId);
 
-        /// <summary>Updates a pet in the store with form data</summary>
-        /// <param name="petId">ID of pet that needs to be updated</param>
-        /// <param name="name">Name of pet that needs to be updated</param>
-        /// <param name="status">Status of pet that needs to be updated</param>
-        /// <returns>A <see cref="Task"/> that completes when the request is finished.</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>405</term>
-        /// <description>Invalid input</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Post("/pet/{petId}")]
         Task UpdatePetWithForm(long petId, [Query] string name, [Query] string status);
 
-        /// <summary>Deletes a pet</summary>
-        /// <param name="petId">Pet id to delete</param>
-        /// <returns>A <see cref="Task"/> that completes when the request is finished.</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>400</term>
-        /// <description>Invalid pet value</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Delete("/pet/{petId}")]
         Task DeletePet(long petId, [Header("api_key")] string api_key);
 
-        /// <summary>uploads an image</summary>
-        /// <param name="petId">ID of pet to update</param>
-        /// <param name="additionalMetadata">Additional Metadata</param>
-        /// <returns>
-        /// A <see cref="Task"/> representing the <see cref="IApiResponse"/> instance containing the result:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>200</term>
-        /// <description>successful operation</description>
-        /// </item>
-        /// </list>
-        /// </returns>
         [Headers("Accept: application/json", "Content-Type: application/octet-stream")]
         [Post("/pet/{petId}/uploadImage")]
         Task<ApiResponse> UploadFile(long petId, [Query] string additionalMetadata, StreamPart body);
 
-        /// <summary>Returns pet inventories by status</summary>
-        /// <remarks>Returns a map of status codes to quantities</remarks>
-        /// <returns>successful operation</returns>
-        /// <exception cref="ApiException">Thrown when the request returns a non-success status code.</exception>
         [Headers("Accept: application/json")]
         [Get("/store/inventory")]
         Task<IDictionary<string, int>> GetInventory();
 
-        /// <summary>Place an order for a pet</summary>
-        /// <remarks>Place a new order in the store</remarks>
-        /// <returns>successful operation</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>405</term>
-        /// <description>Invalid input</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Headers("Accept: application/json", "Content-Type: application/json")]
         [Post("/store/order")]
         Task<Order> PlaceOrder([Body] Order body);
 
-        /// <summary>Find purchase order by ID</summary>
-        /// <remarks>For valid response try integer IDs with value &lt;= 5 or &gt; 10. Other values will generated exceptions</remarks>
-        /// <param name="orderId">ID of order that needs to be fetched</param>
-        /// <returns>successful operation</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>400</term>
-        /// <description>Invalid ID supplied</description>
-        /// </item>
-        /// <item>
-        /// <term>404</term>
-        /// <description>Order not found</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Headers("Accept: application/json")]
         [Get("/store/order/{orderId}")]
         Task<Order> GetOrderById(long orderId);
 
-        /// <summary>Delete purchase order by ID</summary>
-        /// <remarks>For valid response try integer IDs with value &lt; 1000. Anything above 1000 or nonintegers will generate API errors</remarks>
-        /// <param name="orderId">ID of the order that needs to be deleted</param>
-        /// <returns>A <see cref="Task"/> that completes when the request is finished.</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>400</term>
-        /// <description>Invalid ID supplied</description>
-        /// </item>
-        /// <item>
-        /// <term>404</term>
-        /// <description>Order not found</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Delete("/store/order/{orderId}")]
         Task DeleteOrder(long orderId);
 
-        /// <summary>Create user</summary>
-        /// <remarks>This can only be done by the logged in user.</remarks>
-        /// <param name="body">Created user object</param>
-        /// <returns>successful operation</returns>
-        /// <exception cref="ApiException">Thrown when the request returns a non-success status code.</exception>
         [Headers("Accept: application/json, application/xml", "Content-Type: application/json")]
         [Post("/user")]
         Task<User> CreateUser([Body] User body);
 
-        /// <summary>Creates list of users with given input array</summary>
-        /// <remarks>Creates list of users with given input array</remarks>
-        /// <returns>Successful operation</returns>
-        /// <exception cref="ApiException">Thrown when the request returns a non-success status code.</exception>
         [Headers("Accept: application/xml, application/json", "Content-Type: application/json")]
         [Post("/user/createWithList")]
         Task<User> CreateUsersWithListInput([Body] IEnumerable<User> body);
 
-        /// <summary>Logs user into the system</summary>
-        /// <param name="username">The user name for login</param>
-        /// <param name="password">The password for login in clear text</param>
-        /// <returns>successful operation</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>400</term>
-        /// <description>Invalid username/password supplied</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Headers("Accept: application/json")]
         [Get("/user/login")]
         Task<string> LoginUser([Query] string username, [Query] string password);
 
-        /// <summary>Logs out current logged in user session</summary>
-        /// <returns>A <see cref="Task"/> that completes when the request is finished.</returns>
-        /// <exception cref="ApiException">Thrown when the request returns a non-success status code.</exception>
         [Get("/user/logout")]
         Task LogoutUser();
 
-        /// <summary>Get user by user name</summary>
-        /// <param name="username">The name that needs to be fetched. Use user1 for testing.</param>
-        /// <returns>successful operation</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>400</term>
-        /// <description>Invalid username supplied</description>
-        /// </item>
-        /// <item>
-        /// <term>404</term>
-        /// <description>User not found</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Headers("Accept: application/json")]
         [Get("/user/{username}")]
         Task<User> GetUserByName(string username);
 
-        /// <summary>Update user</summary>
-        /// <remarks>This can only be done by the logged in user.</remarks>
-        /// <param name="username">name that need to be deleted</param>
-        /// <param name="body">Update an existent user in the store</param>
-        /// <returns>A <see cref="Task"/> that completes when the request is finished.</returns>
-        /// <exception cref="ApiException">Thrown when the request returns a non-success status code.</exception>
         [Headers("Content-Type: application/json")]
         [Put("/user/{username}")]
         Task UpdateUser(string username, [Body] User body);
 
-        /// <summary>Delete user</summary>
-        /// <remarks>This can only be done by the logged in user.</remarks>
-        /// <param name="username">The name that needs to be deleted</param>
-        /// <returns>A <see cref="Task"/> that completes when the request is finished.</returns>
-        /// <exception cref="ApiException">
-        /// Thrown when the request returns a non-success status code:
-        /// <list type="table">
-        /// <listheader>
-        /// <term>Status</term>
-        /// <description>Description</description>
-        /// </listheader>
-        /// <item>
-        /// <term>400</term>
-        /// <description>Invalid username supplied</description>
-        /// </item>
-        /// <item>
-        /// <term>404</term>
-        /// <description>User not found</description>
-        /// </item>
-        /// </list>
-        /// </exception>
         [Delete("/user/{username}")]
         Task DeleteUser(string username);
 
@@ -736,24 +450,14 @@ namespace Refitter.Tests.AdditionalFiles.SingeInterface
 namespace Refitter.Tests.AdditionalFiles.SingeInterface
 {
     using System;
-   using Microsoft.Extensions.DependencyInjection;
-   using Polly;
-   using Polly.Contrib.WaitAndRetry;
-   using Polly.Extensions.Http;
-   using Refit;
+    using Microsoft.Extensions.DependencyInjection;
+    using Polly;
+    using Polly.Contrib.WaitAndRetry;
+    using Polly.Extensions.Http;
+    using Refit;
 
-    /// <summary>
-    /// Extension methods for configuring Refit clients in the service collection.
-    /// </summary>
     public static partial class IServiceCollectionExtensions
     {
-        /// <summary>
-        /// Configures the Refit clients for dependency injection.
-        /// </summary>
-        /// <param name="services">The service collection to configure.</param>
-        /// <param name="builder">Optional action to configure the HTTP client builder.</param>
-        /// <param name="settings">Optional Refit settings to customize serialization and other behaviors.</param>
-        /// <returns>The configured service collection.</returns>
         public static IServiceCollection ConfigureRefitClients(
             this IServiceCollection services, 
             Action<IHttpClientBuilder>? builder = default, 
