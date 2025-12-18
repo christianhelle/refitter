5a7e82c5bcae0b6ab0055faeb3c01a471194bf31
Linus Hamlin <linus.hamlin@soderbergpartners.se>
2025-12-18 16:35:47 +0100

    Fix input validation

--- src/Refitter/SettingsValidator.cs
+++ src/Refitter/SettingsValidator.cs
@@ -7,29 +7,27 @@ public static class SettingsValidator
 {
     public static ValidationResult Validate(Settings settings)
     {
-        if (BothSettingsFilesAreEmpty(settings) || BothSettingsFilesArePresent(settings))
+        if (string.IsNullOrWhiteSpace(settings.OpenApiPath) &&
+            string.IsNullOrWhiteSpace(settings.SettingsFilePath))
         {
-            return GetValidationErrorForSettingsFiles();
+            return GetValidationErrorForNoInputNoSettings();
         }
 
         return !string.IsNullOrWhiteSpace(settings.SettingsFilePath)
             ? ValidateFilePath(settings)
             : ValidateOperationNameAndUrl(settings);
     }
-
-    private static bool BothSettingsFilesAreEmpty(Settings settings)
-    {
-        return string.IsNullOrWhiteSpace(settings.OpenApiPath) &&
-               string.IsNullOrWhiteSpace(settings.SettingsFilePath);
     }
 
-    private static bool BothSettingsFilesArePresent(Settings settings)
+    private static ValidationResult GetValidationErrorForNoInputNoSettings()
     {
-        return !string.IsNullOrWhiteSpace(settings.OpenApiPath) &&
-               !string.IsNullOrWhiteSpace(settings.SettingsFilePath);
+        return ValidationResult.Error(
+            "You should specify an input URL/file directly " +
+            "or use specify it in 'openApiPath' from the settings file");
     }
 
-    private static ValidationResult GetValidationErrorForSettingsFiles()
+
+    private static ValidationResult GetValidationErrorForTwoInputFiles()
     {
         return ValidationResult.Error(
             "You should either specify an input URL/file directly " +
@@ -41,7 +39,6 @@ public static class SettingsValidator
     {
         var json = File.ReadAllText(settings.SettingsFilePath!);
         var refitGeneratorSettings = Serializer.Deserialize<RefitGeneratorSettings>(json);
-        settings.OpenApiPath = refitGeneratorSettings.OpenApiPath;
 
         return ValidateFileAndOutputSettings(settings, refitGeneratorSettings);
     }
@@ -50,6 +47,14 @@ public static class SettingsValidator
         Settings settings,
         RefitGeneratorSettings refitGeneratorSettings)
     {
+        if (!string.IsNullOrWhiteSpace(settings.OpenApiPath) &&
+            !string.IsNullOrWhiteSpace(refitGeneratorSettings.OpenApiPath))
+        {
+            return GetValidationErrorForTwoInputFiles();
+        }
+
+        settings.OpenApiPath = refitGeneratorSettings.OpenApiPath;
+
         if (string.IsNullOrWhiteSpace(refitGeneratorSettings.OpenApiPath))
         {
             return GetValidationErrorForOpenApiPath();
