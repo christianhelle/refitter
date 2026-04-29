f174ceb0344198b23f9e1dfaff8c894fd288b2b1
Christian Helle <christian.helle@outlook.com>
2026-04-29 22:14:14 +0200

    feat: add build helper and Swagger file utilities for source generator tests

---
+++ src/Refitter.SourceGenerator.Tests/Build/BuildHelper.cs
@@ -0,0 +1,70 @@
+using System.Diagnostics;
+using System.Text;
+
+namespace Refitter.SourceGenerators.Tests.Build;
+
+public static class BuildHelper
+{
+    public static bool BuildCSharp(params string[] generatedCode)
+    {
+        var folder = Path.GetDirectoryName(typeof(BuildHelper).Assembly.Location) ?? Path.GetTempPath();
+        var path = Path.Combine(folder, Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(path);
+        var projectFile = Path.Combine(path, "Project.csproj");
+        File.WriteAllText(projectFile, ProjectFileContents.Net80App);
+
+        for (int i = 0; i < generatedCode.Length; i++)
+        {
+            File.WriteAllText(Path.Combine(path, $"Generated_{i}.cs"), generatedCode[i]);
+        }
+
+        var processStartInfo = new ProcessStartInfo(GetDotNetCli(), $"build \"{projectFile}\"");
+        processStartInfo.RedirectStandardOutput = true;
+        processStartInfo.RedirectStandardError = true;
+        processStartInfo.UseShellExecute = false;
+        processStartInfo.CreateNoWindow = true;
+
+        var process = new Process();
+        process.StartInfo = processStartInfo;
+
+        var output = new StringBuilder();
+        process.OutputDataReceived += (_, args) => output.AppendLine(args.Data);
+
+        var errors = new StringBuilder();
+        process.ErrorDataReceived += (_, args) => errors.AppendLine(args.Data);
+
+        bool startResult = process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+        process.WaitForExit();
+
+        var result = startResult && process.ExitCode == 0;
+
+        if (!result)
+            throw new BuildFailedException(errors.ToString(), output.ToString());
+
+        try
+        {
+            Directory.Delete(path, true);
+        }
+        catch
+        {
+            // Ignore cleanup errors
+        }
+
+        return result;
+    }
+
+    private static string GetDotNetCli()
+    {
+        return Environment.OSVersion.Platform is PlatformID.Unix or PlatformID.MacOSX
+            ? "dotnet"
+            : "dotnet.exe";
+    }
+}
+
+public class BuildFailedException(string Errors, string Output)
+    : Exception($"Build failed{Crlf}Errors:{Crlf}{Errors}{Crlf}Output:{Crlf}{Output}")
+{
+    private static readonly string Crlf = Environment.NewLine;
+}
---
+++ src/Refitter.SourceGenerator.Tests/Build/ProjectFileContents.cs
@@ -0,0 +1,31 @@
+namespace Refitter.SourceGenerators.Tests.Build;
+
+public static class ProjectFileContents
+{
+    public const string Net80App = @"
+<Project Sdk=""Microsoft.NET.Sdk"">
+  <PropertyGroup>
+    <TargetFramework>net8.0</TargetFramework>
+    <GenerateDocumentationFile>true</GenerateDocumentationFile>
+  </PropertyGroup>
+  <ItemGroup>
+    <PackageReference Include=""Refit.HttpClientFactory"" Version=""8.0.0"" />
+    <PackageReference Include=""System.Text.Json"" Version=""8.0.5"" />
+    <PackageReference Include=""System.ComponentModel.Annotations"" Version=""4.5.0"" />
+    <PackageReference Include=""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
+    <PackageReference Include=""Microsoft.Extensions.DependencyInjection"" Version=""8.0.1"" />
+    <PackageReference Include=""Microsoft.Extensions.Http.Polly"" Version=""8.0.11"" />
+    <PackageReference Include=""Microsoft.Extensions.Http.Resilience"" Version=""8.10.0"" />
+    <PackageReference Include=""Microsoft.Extensions.Options.ConfigurationExtensions"" Version=""8.0.0"" />
+    <PackageReference Include=""Polly.Contrib.WaitAndRetry"" Version=""1.1.1"" />
+    <PackageReference Include=""System.Reactive"" Version=""6.0.1"" />
+    <PackageReference Include=""Apizr.Integrations.FileTransfer.MediatR"" Version=""6.4.0"" />
+    <PackageReference Include=""Apizr.Integrations.Mapster"" Version=""6.4.0"" />
+    <PackageReference Include=""Apizr.Integrations.AutoMapper"" Version=""6.4.0"" />
+    <PackageReference Include=""Apizr.Integrations.Akavache"" Version=""6.4.0"" />
+    <PackageReference Include=""Apizr.Integrations.MonkeyCache"" Version=""6.4.0"" />
+    <PackageReference Include=""Apizr.Extensions.Microsoft.Caching"" Version=""6.4.0"" />
+    <PackageReference Include=""Apizr.Integrations.Fusillade"" Version=""6.4.0"" />
+  </ItemGroup>
+</Project>";
+}
