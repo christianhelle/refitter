8a1c0c9bbf08aa0fb08683b2cac6c8ec678dd7ce
Christian Helle <christian.helle@outlook.com>
2026-06-15 00:06:40 +0200

    test: add unit tests for ReturnTypeGenerator, MethodAttributeGenerator, and MethodSignatureGenerator
    - ReturnTypeGeneratorTests: 10 tests covering return type derivation,
      IApiResponse wrapping, Task/IObservable selection, file stream responses,
      response type override, IsApiResponseType
    - MethodAttributeGeneratorTests: 8 tests covering Accept/Content-Type/
      Authorization headers, [Multipart], [Obsolete], and combinations
    - MethodSignatureGeneratorTests: 7 tests covering parameter aggregation,
      CancellationToken, dynamic querystring, Apizr RequestOptions

---
+++ src/Refitter.Tests/MethodAttributeGeneratorTests.cs
@@ -0,0 +1,297 @@
+using FluentAssertions;
+using NSwag;
+using Refitter.Core;
+using TUnit.Core;
+
+namespace Refitter.Tests;
+
+public class MethodAttributeGeneratorTests
+{
+    [Test]
+    public async Task Generate_Includes_Obsolete_When_Deprecated()
+    {
+        var spec = """
+            openapi: 3.0.0
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /test:
+                get:
+                  operationId: getTest
+                  deprecated: true
+                  responses:
+                    '200':
+                      description: Success
+            """;
+
+        var document = await OpenApiYamlDocument.FromYamlAsync(spec);
+        var settings = new RefitGeneratorSettings();
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new MethodAttributeGenerator(settings, document);
+
+        var operation = document.Paths["/test"]["get"];
+        var operationModel = generator.CreateOperationModel(operation);
+        var attributes = sut.Generate(operation, operationModel);
+
+        attributes.Should().Contain("[System.Obsolete]");
+    }
+
+    [Test]
+    public async Task Generate_Includes_Multipart_For_MultipartFormData()
+    {
+        var spec = """
+            openapi: 3.0.0
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /test:
+                post:
+                  operationId: uploadFile
+                  requestBody:
+                    required: true
+                    content:
+                      multipart/form-data:
+                        schema:
+                          type: object
+                          properties:
+                            file:
+                              type: string
+                              format: binary
+                  responses:
+                    '200':
+                      description: Success
+            """;
+
+        var document = await OpenApiYamlDocument.FromYamlAsync(spec);
+        var settings = new RefitGeneratorSettings();
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new MethodAttributeGenerator(settings, document);
+
+        var operation = document.Paths["/test"]["post"];
+        var operationModel = generator.CreateOperationModel(operation);
+        var attributes = sut.Generate(operation, operationModel);
+
+        attributes.Should().Contain("[Multipart]");
+    }
+
+    [Test]
+    public async Task Generate_Includes_Accept_Header_When_Enabled()
+    {
+        var spec = """
+            openapi: 3.0.0
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /test:
+                get:
+                  operationId: getTest
+                  responses:
+                    '200':
+                      description: Success
+                      content:
+                        application/json:
+                          schema:
+                            type: string
+            """;
+
+        var document = await OpenApiYamlDocument.FromYamlAsync(spec);
+        var settings = new RefitGeneratorSettings { AddAcceptHeaders = true };
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new MethodAttributeGenerator(settings, document);
+
+        var operation = document.Paths["/test"]["get"];
+        var operationModel = generator.CreateOperationModel(operation);
+        var attributes = sut.Generate(operation, operationModel);
+
+        attributes.Should().Contain(a => a.Contains("Accept"));
+    }
+
+    [Test]
+    public async Task Generate_Includes_ContentType_Header_When_Enabled()
+    {
+        var spec = """
+            openapi: 3.0.0
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /test:
+                post:
+                  operationId: createTest
+                  requestBody:
+                    required: true
+                    content:
+                      application/json:
+                        schema:
+                          type: object
+                          properties:
+                            name:
+                              type: string
+                  responses:
+                    '201':
+                      description: Created
+            """;
+
+        var document = await OpenApiYamlDocument.FromYamlAsync(spec);
+        var settings = new RefitGeneratorSettings { AddContentTypeHeaders = true };
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new MethodAttributeGenerator(settings, document);
+
+        var operation = document.Paths["/test"]["post"];
+        var operationModel = generator.CreateOperationModel(operation);
+        var attributes = sut.Generate(operation, operationModel);
+
+        attributes.Should().Contain(a => a.Contains("Content-Type"));
+    }
+
+    [Test]
+    public async Task Generate_Omits_Accept_Header_When_Disabled()
+    {
+        var spec = """
+            openapi: 3.0.0
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /test:
+                get:
+                  operationId: getTest
+                  responses:
+                    '200':
+                      description: Success
+                      content:
+                        application/json:
+                          schema:
+                            type: string
+            """;
+
+        var document = await OpenApiYamlDocument.FromYamlAsync(spec);
+        var settings = new RefitGeneratorSettings { AddAcceptHeaders = false };
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new MethodAttributeGenerator(settings, document);
+
+        var operation = document.Paths["/test"]["get"];
+        var operationModel = generator.CreateOperationModel(operation);
+        var attributes = sut.Generate(operation, operationModel);
+
+        attributes.Should().NotContain(a => a.Contains("Accept"));
+    }
+
+    [Test]
+    public async Task Generate_Includes_Authorization_Header_For_Bearer_Auth()
+    {
+        var spec = """
+            openapi: 3.0.0
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /test:
+                get:
+                  operationId: getTest
+                  security:
+                    - bearerAuth: []
+                  responses:
+                    '200':
+                      description: Success
+            components:
+              securitySchemes:
+                bearerAuth:
+                  type: http
+                  scheme: bearer
+            """;
+
+        var document = await OpenApiYamlDocument.FromYamlAsync(spec);
+        var settings = new RefitGeneratorSettings
+        {
+            AuthenticationHeaderStyle = AuthenticationHeaderStyle.Method
+        };
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new MethodAttributeGenerator(settings, document);
+
+        var operation = document.Paths["/test"]["get"];
+        var operationModel = generator.CreateOperationModel(operation);
+        var attributes = sut.Generate(operation, operationModel);
+
+        attributes.Should().Contain(a => a.Contains("Authorization: Bearer"));
+    }
+
+    [Test]
+    public async Task Generate_Omits_ContentType_For_Multipart()
+    {
+        var spec = """
+            openapi: 3.0.0
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /test:
+                post:
+                  operationId: uploadFile
+                  requestBody:
+                    required: true
+                    content:
+                      multipart/form-data:
+                        schema:
+                          type: object
+                          properties:
+                            file:
+                              type: string
+                              format: binary
+                  responses:
+                    '200':
+                      description: Success
+            """;
+
+        var document = await OpenApiYamlDocument.FromYamlAsync(spec);
+        var settings = new RefitGeneratorSettings { AddContentTypeHeaders = true };
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new MethodAttributeGenerator(settings, document);
+
+        var operation = document.Paths["/test"]["post"];
+        var operationModel = generator.CreateOperationModel(operation);
+        var attributes = sut.Generate(operation, operationModel);
+
+        attributes.Should().NotContain(a => a.Contains("Content-Type"));
+    }
+
+    [Test]
+    public async Task Generate_Returns_Empty_When_No_Attributes()
+    {
+        var spec = """
+            openapi: 3.0.0
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /test:
+                get:
+                  operationId: getTest
+                  responses:
+                    '200':
+                      description: Success
+                      content:
+                        application/json:
+                          schema:
+                            type: string
+            """;
+
+        var document = await OpenApiYamlDocument.FromYamlAsync(spec);
+        var settings = new RefitGeneratorSettings
+        {
+            AddAcceptHeaders = false,
+            AddContentTypeHeaders = false
+        };
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new MethodAttributeGenerator(settings, document);
+
+        var operation = document.Paths["/test"]["get"];
+        var operationModel = generator.CreateOperationModel(operation);
+        var attributes = sut.Generate(operation, operationModel);
+
+        attributes.Should().BeEmpty();
+    }
+}
---
+++ src/Refitter.Tests/ReturnTypeGeneratorTests.cs
@@ -0,0 +1,379 @@
+using FluentAssertions;
+using NSwag;
+using Refitter.Core;
+using TUnit.Core;
+
+namespace Refitter.Tests;
+
+public class ReturnTypeGeneratorTests
+{
+    [Test]
+    public async Task Generate_Returns_Task_For_Void_Response()
+    {
+        var spec = """
+            {
+              "openapi": "3.0.0",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/test": {
+                  "get": {
+                    "operationId": "getTest",
+                    "responses": {
+                      "204": { "description": "No Content" }
+                    }
+                  }
+                }
+              }
+            }
+            """;
+
+        var document = await OpenApiDocument.FromJsonAsync(spec);
+        var settings = new RefitGeneratorSettings();
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new ReturnTypeGenerator(settings, generator);
+
+        var operation = document.Paths["/test"]["get"];
+        var result = sut.Generate(operation);
+
+        result.Should().Be("Task");
+    }
+
+    [Test]
+    public async Task Generate_Returns_Task_Of_Type_For_Success_Response()
+    {
+        var spec = """
+            {
+              "openapi": "3.0.0",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/test": {
+                  "get": {
+                    "operationId": "getTest",
+                    "responses": {
+                      "200": {
+                        "description": "Success",
+                        "content": {
+                          "application/json": {
+                            "schema": {
+                              "type": "string"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """;
+
+        var document = await OpenApiDocument.FromJsonAsync(spec);
+        var settings = new RefitGeneratorSettings();
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new ReturnTypeGenerator(settings, generator);
+
+        var operation = document.Paths["/test"]["get"];
+        var result = sut.Generate(operation);
+
+        result.Should().Be("Task<string>");
+    }
+
+    [Test]
+    public async Task Generate_Returns_IApiResponse_When_Wrapping_Enabled()
+    {
+        var spec = """
+            {
+              "openapi": "3.0.0",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/test": {
+                  "get": {
+                    "operationId": "getTest",
+                    "responses": {
+                      "200": {
+                        "description": "Success",
+                        "content": {
+                          "application/json": {
+                            "schema": {
+                              "type": "string"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """;
+
+        var document = await OpenApiDocument.FromJsonAsync(spec);
+        var settings = new RefitGeneratorSettings { ReturnIApiResponse = true };
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new ReturnTypeGenerator(settings, generator);
+
+        var operation = document.Paths["/test"]["get"];
+        var result = sut.Generate(operation);
+
+        result.Should().Be("Task<IApiResponse<string>>");
+    }
+
+    [Test]
+    public async Task Generate_Returns_IObservable_When_Observable_Enabled()
+    {
+        var spec = """
+            {
+              "openapi": "3.0.0",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/test": {
+                  "get": {
+                    "operationId": "getTest",
+                    "responses": {
+                      "200": {
+                        "description": "Success",
+                        "content": {
+                          "application/json": {
+                            "schema": {
+                              "type": "string"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """;
+
+        var document = await OpenApiDocument.FromJsonAsync(spec);
+        var settings = new RefitGeneratorSettings { ReturnIObservable = true };
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new ReturnTypeGenerator(settings, generator);
+
+        var operation = document.Paths["/test"]["get"];
+        var result = sut.Generate(operation);
+
+        result.Should().Be("IObservable<string>");
+    }
+
+    [Test]
+    public async Task IsFileStreamResponse_Returns_True_For_Binary_Content()
+    {
+        var spec = """
+            {
+              "openapi": "3.0.0",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/test": {
+                  "get": {
+                    "operationId": "getFile",
+                    "responses": {
+                      "200": {
+                        "description": "File",
+                        "content": {
+                          "application/octet-stream": {
+                            "schema": {
+                              "type": "string",
+                              "format": "binary"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """;
+
+        var document = await OpenApiDocument.FromJsonAsync(spec);
+        var settings = new RefitGeneratorSettings();
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new ReturnTypeGenerator(settings, generator);
+
+        var operation = document.Paths["/test"]["get"];
+        var result = sut.IsFileStreamResponse(operation);
+
+        result.Should().BeTrue();
+    }
+
+    [Test]
+    public async Task IsFileStreamResponse_Returns_False_For_Json_Content()
+    {
+        var spec = """
+            {
+              "openapi": "3.0.0",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/test": {
+                  "get": {
+                    "operationId": "getTest",
+                    "responses": {
+                      "200": {
+                        "description": "Success",
+                        "content": {
+                          "application/json": {
+                            "schema": {
+                              "type": "string"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """;
+
+        var document = await OpenApiDocument.FromJsonAsync(spec);
+        var settings = new RefitGeneratorSettings();
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new ReturnTypeGenerator(settings, generator);
+
+        var operation = document.Paths["/test"]["get"];
+        var result = sut.IsFileStreamResponse(operation);
+
+        result.Should().BeFalse();
+    }
+
+    [Test]
+    public async Task Generate_Returns_FileStream_Response_Type()
+    {
+        var spec = """
+            {
+              "openapi": "3.0.0",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/test": {
+                  "get": {
+                    "operationId": "getFile",
+                    "responses": {
+                      "200": {
+                        "description": "File",
+                        "content": {
+                          "application/pdf": {
+                            "schema": {
+                              "type": "string",
+                              "format": "binary"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """;
+
+        var document = await OpenApiDocument.FromJsonAsync(spec);
+        var settings = new RefitGeneratorSettings();
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new ReturnTypeGenerator(settings, generator);
+
+        var operation = document.Paths["/test"]["get"];
+        var result = sut.Generate(operation);
+
+        result.Should().Be("Task<HttpResponseMessage>");
+    }
+
+    [Test]
+    public async Task IsApiResponseType_Detects_Task_Of_HttpResponseMessage()
+    {
+        var settings = new RefitGeneratorSettings();
+        var generator = new CSharpClientGeneratorFactory(
+            settings,
+            await OpenApiDocument.FromJsonAsync("""
+                { "openapi": "3.0.0", "info": { "title": "Test", "version": "1.0" }, "paths": {} }
+                """))
+            .Create();
+        var sut = new ReturnTypeGenerator(settings, generator);
+
+        sut.IsApiResponseType("Task<HttpResponseMessage>").Should().BeTrue();
+        sut.IsApiResponseType("IObservable<HttpResponseMessage>").Should().BeTrue();
+        sut.IsApiResponseType("Task<string>").Should().BeFalse();
+        sut.IsApiResponseType("Task<IApiResponse>").Should().BeTrue();
+        sut.IsApiResponseType("Task<IApiResponse<string>>").Should().BeTrue();
+    }
+
+    [Test]
+    public async Task Generate_With_ResponseTypeOverride_Uses_Custom_Type()
+    {
+        var spec = """
+            {
+              "openapi": "3.0.0",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/test": {
+                  "get": {
+                    "operationId": "customOp",
+                    "responses": {
+                      "200": {
+                        "description": "Success",
+                        "content": {
+                          "application/json": {
+                            "schema": { "type": "string" }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """;
+
+        var document = await OpenApiDocument.FromJsonAsync(spec);
+        var settings = new RefitGeneratorSettings();
+        settings.ResponseTypeOverride["customOp"] = "MyCustomType";
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new ReturnTypeGenerator(settings, generator);
+
+        var operation = document.Paths["/test"]["get"];
+        var result = sut.Generate(operation);
+
+        result.Should().Be("Task<MyCustomType>");
+    }
+
+    [Test]
+    public async Task Generate_With_ResponseTypeOverride_Void_Returns_Task()
+    {
+        var spec = """
+            {
+              "openapi": "3.0.0",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/test": {
+                  "get": {
+                    "operationId": "customOp",
+                    "responses": {
+                      "200": {
+                        "description": "Success",
+                        "content": {
+                          "application/json": {
+                            "schema": { "type": "string" }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """;
+
+        var document = await OpenApiDocument.FromJsonAsync(spec);
+        var settings = new RefitGeneratorSettings();
+        settings.ResponseTypeOverride["customOp"] = "void";
+        var generator = new CSharpClientGeneratorFactory(settings, document).Create();
+        var sut = new ReturnTypeGenerator(settings, generator);
+
+        var operation = document.Paths["/test"]["get"];
+        var result = sut.Generate(operation);
+
+        result.Should().Be("Task");
+    }
+}
