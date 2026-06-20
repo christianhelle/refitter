4bd4c14e430521810e4ad28eb2dc5e2f8abf7e1e
Christian Helle <christian.helle@outlook.com>
2026-06-20 17:26:54 +0200

    chore: remove unused imports from source generator
    System.Runtime.CompilerServices and System.Text were only used by
    the old WriteGeneratedFile method and are no longer needed.

--- src/Refitter.SourceGenerator/RefitterSourceGenerator.cs
+++ src/Refitter.SourceGenerator/RefitterSourceGenerator.cs
@@ -1,7 +1,5 @@
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using System.Text;
 using H.Generators.Extensions;
 using Microsoft.CodeAnalysis;
 using Refitter.Core;
