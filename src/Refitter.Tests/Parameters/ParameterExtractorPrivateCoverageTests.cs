070ced652a6b996c0b1fea21d3668608bc41a5a6
Christian Helle <christian.helle@outlook.com>
2026-06-21 10:42:58 +0200

    refactor: remove reflection-dependent tests from ParameterExtractorPrivateCoverageTests
    Delete 15 tests that used reflection-based CreateParameterModel/CreateOperationModel
    helpers (RuntimeHelpers.GetUninitializedObject + BindingFlags) to construct NSwag types.
    Also remove the helper methods and unused reflection imports.
    
    Keep 20 tests that test ParameterShared utility methods directly via InternalsVisibleTo
    using only NJsonSchema types and simple objects.

--- src/Refitter.Tests/Parameters/ParameterExtractorPrivateCoverageTests.cs
+++ src/Refitter.Tests/Parameters/ParameterExtractorPrivateCoverageTests.cs
@@ -1,11 +1,7 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Text;
 using FluentAssertions;
 using NJsonSchema;
-using NSwag;
 using NSwag.CodeGeneration.CSharp.Models;
-using NSwag.CodeGeneration.Models;
 using Refitter.Core;
 
 namespace Refitter.Tests.Parameters;
@@ -93,32 +89,6 @@ public class ParameterExtractorPrivateCoverageTests
     }
 
     [Test]
-    public void GetAliasAsAttribute_CSharpParameterModel_Returns_Expected_Value()
-    {
-        var unchangedName = CreateParameterModel("id", "id");
-        var aliasedName = CreateParameterModel("user-id", "userId");
-
-        var unchangedResult = ParameterShared.GetAliasAsAttribute(unchangedName);
-        var aliasedResult = ParameterShared.GetAliasAsAttribute(aliasedName);
-
-        unchangedResult.Should().BeEmpty();
-        aliasedResult.Should().Be("AliasAs(\"user-id\")");
-    }
-
-    [Test]
-    public void GetAliasAsAttribute_CSharpParameterModel_Escapes_Special_Characters()
-    {
-        var withQuote = CreateParameterModel("user\"id", "userQuoteid");
-        var withBackslash = CreateParameterModel("user\\id", "userBackslashid");
-
-        var quoteResult = ParameterShared.GetAliasAsAttribute(withQuote);
-        var backslashResult = ParameterShared.GetAliasAsAttribute(withBackslash);
-
-        quoteResult.Should().Be("AliasAs(\"user\\\"id\")");
-        backslashResult.Should().Be("AliasAs(\"user\\\\id\")");
-    }
-
-    [Test]
     public void GetAliasAsAttribute_StringOverload_Escapes_Special_Characters()
     {
         var unchanged = ParameterShared.GetAliasAsAttribute("same", "same");
@@ -185,77 +155,6 @@ public class ParameterExtractorPrivateCoverageTests
     }
 
     [Test]
-    public void ParameterAggregator_ExtractParameters_Adds_Multipart_Text_Fields_When_NSwag_Parameters_Are_Empty()
-    {
-        var operationModel = CreateOperationModel();
-        var operation = new OpenApiOperation
-        {
-            RequestBody = new OpenApiRequestBody()
-        };
-        var schema = new JsonSchema();
-        schema.Properties["title"] = new JsonSchemaProperty
-        {
-            Type = JsonObjectType.String
-        };
-        operation.RequestBody.Content["multipart/form-data"] = new OpenApiMediaType
-        {
-            Schema = schema
-        };
-
-        var aggregator = new ParameterAggregator();
-        var result = aggregator.ExtractParameters(
-            operationModel,
-            operation,
-            new RefitGeneratorSettings(),
-            "QueryParams",
-            out var dynamicQuerystringParameters);
-
-        result.Should().ContainSingle().Which.Should().Be("string title");
-        dynamicQuerystringParameters.Should().Be(string.Empty);
-    }
-
-    [Test]
-    public void ParameterAggregator_Does_Not_Mutate_Query_Parameter_Collection_When_Generating_Dynamic_Querystring_Wrapper()
-    {
-        var firstParameter = CreateParameterModel(
-            "query",
-            "query",
-            parameter: new OpenApiParameter
-            {
-                Name = "query",
-                Kind = OpenApiParameterKind.Query,
-                IsRequired = true,
-                Schema = new JsonSchema { Type = JsonObjectType.String }
-            });
-        var secondParameter = CreateParameterModel(
-            "page",
-            "page",
-            type: "int?",
-            parameter: new OpenApiParameter
-            {
-                Name = "page",
-                Kind = OpenApiParameterKind.Query,
-                Schema = new JsonSchema { Type = JsonObjectType.Integer }
-            });
-        var operationModel = CreateOperationModel(firstParameter, secondParameter);
-        var operation = new OpenApiOperation();
-
-        var aggregator = new ParameterAggregator();
-        var parameters = aggregator.ExtractParameters(
-                operationModel,
-                operation,
-                new RefitGeneratorSettings { UseDynamicQuerystringParameters = true },
-                "SearchQueryParams",
-                out var dynamicQuerystringParameters)
-            .ToList();
-
-        parameters.Should().ContainSingle().Which.Should().Be("[Query] SearchQueryParams queryParams");
-        dynamicQuerystringParameters.Should().Contain("class SearchQueryParams");
-        operationModel.Parameters.Should().HaveCount(2);
-        operationModel.Parameters.Should().ContainInOrder(firstParameter, secondParameter);
-    }
-
-    [Test]
     public void ReplaceUnsafeCharacters_Delegates_To_ParameterShared()
     {
         var result = ParameterShared.ReplaceUnsafeCharacters("unsafe-name!");
@@ -282,30 +181,6 @@ public class ParameterExtractorPrivateCoverageTests
     }
 
     [Test]
-    public void GetBodyAttribute_Returns_Body_For_String_Parameter()
-    {
-        var param = CreateParameterModel("body", "body", "string");
-        var result = ParameterShared.GetBodyAttribute(param, new RefitGeneratorSettings());
-        result.Should().Be("Body");
-    }
-
-    [Test]
-    public void GetBodyAttribute_Returns_Serialized_For_Object_Parameter()
-    {
-        var param = CreateParameterModel("body", "body", "object");
-        var result = ParameterShared.GetBodyAttribute(param, new RefitGeneratorSettings());
-        result.Should().Be("Body(BodySerializationMethod.Serialized)");
-    }
-
-    [Test]
-    public void GetQueryAttribute_Returns_Query_Attribute()
-    {
-        var param = CreateParameterModel("q", "q");
-        var result = ParameterShared.GetQueryAttribute(param, new RefitGeneratorSettings());
-        result.Should().NotBeNull();
-    }
-
-    [Test]
     public void JoinAttributes_Returns_Empty_For_Empty_Input()
     {
         var result = ParameterShared.JoinAttributes();
@@ -324,22 +199,6 @@ public class ParameterExtractorPrivateCoverageTests
     {
         var result = ParameterShared.JoinAttributes("AliasAs(\"name\")");
         result.Should().Be("[AliasAs(\"name\")] ");
-    }
-
-    [Test]
-    public void GetParameterType_Uses_CSharpProvider_Model_Type()
-    {
-        var param = CreateParameterModel("param1", "param1", "int");
-        var result = ParameterShared.GetParameterType(param, new RefitGeneratorSettings());
-        result.Should().Be("int");
-    }
-
-    [Test]
-    public void GetQueryParameterType_Returns_Query_Parameter_Type()
-    {
-        var param = CreateParameterModel("q", "q", "string");
-        var result = ParameterShared.GetQueryParameterType(param, new RefitGeneratorSettings());
-        result.Should().NotBeNull();
     }
 
     [Test]
@@ -364,192 +223,10 @@ public class ParameterExtractorPrivateCoverageTests
     }
 
     [Test]
-    public void GetVariableName_Returns_VariableName_From_Model()
-    {
-        var param = CreateParameterModel("param-name", "paramName");
-        var result = ParameterShared.GetVariableName(param);
-        result.Should().Be("paramName");
-    }
-
-    [Test]
     public void AppendXmlDocComment_Extends_CodeBuilder()
     {
         var codeBuilder = new StringBuilder();
         ParameterShared.AppendXmlDocComment("Some description", codeBuilder);
         codeBuilder.ToString().Should().Contain("Some description");
-    }
-
-    [Test]
-    public void QueryParameterExtractor_Returns_Empty_When_No_Query_Parameters()
-    {
-        var operationModel = CreateOperationModel();
-        var extractor = new QueryParameterExtractor();
-        var result = extractor.Extract(operationModel, new OpenApiOperation(), new RefitGeneratorSettings()).ToList();
-
-        result.Should().BeEmpty();
-        extractor.DynamicQuerystringCode.Should().Be(string.Empty);
-    }
-
-    [Test]
-    public void QueryParameterExtractor_With_Dynamic_Enabled_But_Single_Query_Param_Falls_Back_To_Simple()
-    {
-        var param = CreateParameterModel("query", "query", "string",
-            parameter: new OpenApiParameter
-            {
-                Name = "query",
-                Kind = OpenApiParameterKind.Query,
-                Schema = new JsonSchema { Type = JsonObjectType.String }
-            });
-        var operationModel = CreateOperationModel(param);
-        var settings = new RefitGeneratorSettings { UseDynamicQuerystringParameters = true };
-
-        var extractor = new QueryParameterExtractor
-        {
-            DynamicQuerystringParameterType = "QueryParams"
-        };
-        var result = extractor.Extract(operationModel, new OpenApiOperation(), settings).ToList();
-
-        result.Should().ContainSingle().Which.Should().Contain("query");
-        extractor.DynamicQuerystringCode.Should().Be(string.Empty);
-    }
-
-    [Test]
-    public void QueryParameterExtractor_With_No_Dynamic_Returns_Simple_Extraction()
-    {
-        var param = CreateParameterModel("query", "query", "string",
-            parameter: new OpenApiParameter
-            {
-                Name = "query",
-                Kind = OpenApiParameterKind.Query,
-                Schema = new JsonSchema { Type = JsonObjectType.String }
-            });
-        var operationModel = CreateOperationModel(param);
-        var settings = new RefitGeneratorSettings();
-
-        var extractor = new QueryParameterExtractor();
-        var result = extractor.Extract(operationModel, new OpenApiOperation(), settings).ToList();
-
-        result.Should().ContainSingle().Which.Should().Contain("query");
-        extractor.DynamicQuerystringCode.Should().Be(string.Empty);
-    }
-
-    [Test]
-    public void QueryParameterExtractor_With_Dynamic_And_All_Nullable()
-    {
-        var firstParameter = CreateParameterModel(
-            "query",
-            "query",
-            type: "string?",
-            parameter: new OpenApiParameter
-            {
-                Name = "query",
-                Kind = OpenApiParameterKind.Query,
-                Schema = new JsonSchema { Type = JsonObjectType.String }
-            });
-        var secondParameter = CreateParameterModel(
-            "page",
-            "page",
-            type: "int?",
-            parameter: new OpenApiParameter
-            {
-                Name = "page",
-                Kind = OpenApiParameterKind.Query,
-                Schema = new JsonSchema { Type = JsonObjectType.Integer }
-            });
-        var operationModel = CreateOperationModel(firstParameter, secondParameter);
-
-        var extractor = new QueryParameterExtractor
-        {
-            DynamicQuerystringParameterType = "SearchQueryParams"
-        };
-        var result = extractor.Extract(
-            operationModel,
-            new OpenApiOperation(),
-            new RefitGeneratorSettings { UseDynamicQuerystringParameters = true }).ToList();
-
-        result.Should().ContainSingle().Which.Should().Be("[Query] SearchQueryParams? queryParams");
-        extractor.DynamicQuerystringCode.Should().Contain("class SearchQueryParams");
-    }
-
-    [Test]
-    public void QueryParameterExtractor_With_Dynamic_And_Not_All_Nullable()
-    {
-        var firstParameter = CreateParameterModel(
-            "query",
-            "query",
-            type: "string",
-            parameter: new OpenApiParameter
-            {
-                Name = "query",
-                Kind = OpenApiParameterKind.Query,
-                Schema = new JsonSchema { Type = JsonObjectType.String }
-            });
-        var secondParameter = CreateParameterModel(
-            "page",
-            "page",
-            type: "int",
-            parameter: new OpenApiParameter
-            {
-                Name = "page",
-                Kind = OpenApiParameterKind.Query,
-                Schema = new JsonSchema { Type = JsonObjectType.Integer }
-            });
-        var operationModel = CreateOperationModel(firstParameter, secondParameter);
-
-        var extractor = new QueryParameterExtractor
-        {
-            DynamicQuerystringParameterType = "SearchQueryParams"
-        };
-        var result = extractor.Extract(
-            operationModel,
-            new OpenApiOperation(),
-            new RefitGeneratorSettings { UseDynamicQuerystringParameters = true }).ToList();
-
-        result.Should().ContainSingle().Which.Should().Be("[Query] SearchQueryParams queryParams");
-        extractor.DynamicQuerystringCode.Should().Contain("class SearchQueryParams");
-    }
-
-    private static CSharpParameterModel CreateParameterModel(
-        string name,
-        string variableName,
-        string type = "string",
-        OpenApiParameter? parameter = null)
-    {
-        var parameterModel = (CSharpParameterModel)RuntimeHelpers.GetUninitializedObject(typeof(CSharpParameterModel));
-        var baseType = typeof(CSharpParameterModel).BaseType!;
-
-        baseType
-            .GetField("<Type>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)!
-            .SetValue(parameterModel, type);
-        baseType
-            .GetField("<Name>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)!
-            .SetValue(parameterModel, name);
-        baseType
-            .GetField("<VariableName>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)!
-            .SetValue(parameterModel, variableName);
-        baseType
-            .GetField("_parameter", BindingFlags.Instance | BindingFlags.NonPublic)!
-            .SetValue(
-                parameterModel,
-                parameter ?? new OpenApiParameter
-                {
-                    Name = name,
-                    Kind = OpenApiParameterKind.Query,
-                    Schema = new JsonSchema()
-                });
-
-        return parameterModel;
-    }
-
-    private static CSharpOperationModel CreateOperationModel(params CSharpParameterModel[] parameters)
-    {
-        var operationModel = (CSharpOperationModel)RuntimeHelpers.GetUninitializedObject(typeof(CSharpOperationModel));
-        var baseType = typeof(CSharpOperationModel).BaseType!;
-
-        baseType
-            .GetField("<Parameters>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)!
-            .SetValue(operationModel, parameters.ToList());
-
-        return operationModel;
     }
 }
