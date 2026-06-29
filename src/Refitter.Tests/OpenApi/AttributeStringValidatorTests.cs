83d4d69c48d2f12debf9489b9844a5c6f56f7705
Christian Helle <christian.helle@outlook.com>
2026-06-29 17:35:25 +0200

    test: attribute string validator

---
+++ src/Refitter.Tests/OpenApi/AttributeStringValidatorTests.cs
@@ -0,0 +1,130 @@
+using FluentAssertions;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Reader;
+using Refitter.Core.Validation;
+using System.Net.Http;
+
+namespace Refitter.Tests.OpenApi;
+
+public class AttributeStringValidatorTests
+{
+    [Test]
+    public void ContainsUnsafeCharacters_Should_Return_False_For_Null_And_Safe_Values()
+    {
+        AttributeStringValidator.ContainsUnsafeCharacters(null).Should().BeFalse();
+        AttributeStringValidator.ContainsUnsafeCharacters("X-Api-Key").Should().BeFalse();
+    }
+
+    [Test]
+    public void ContainsUnsafeCharacters_Should_Return_True_For_Unsafe_Values()
+    {
+        AttributeStringValidator.ContainsUnsafeCharacters("X\"Api").Should().BeTrue();
+        AttributeStringValidator.ContainsUnsafeCharacters("X\\Api").Should().BeTrue();
+        AttributeStringValidator.ContainsUnsafeCharacters("X\nApi").Should().BeTrue();
+    }
+
+    [Test]
+    public void Validate_Should_Return_When_Document_Is_Null()
+    {
+        OpenApiDiagnostic diagnostic = new();
+
+        AttributeStringValidator.Validate(null, diagnostic);
+
+        diagnostic.Errors.Should().BeEmpty();
+    }
+
+    [Test]
+    public void Validate_Should_Validate_SecurityScheme_And_Return_When_Paths_Are_Null()
+    {
+        OpenApiDiagnostic diagnostic = new();
+        OpenApiDocument document = new()
+        {
+            Components = new OpenApiComponents
+            {
+                SecuritySchemes = new Dictionary<string, IOpenApiSecurityScheme>
+                {
+                    ["nullScheme"] = null!,
+                    ["unsafeHeader"] = new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.ApiKey,
+                        In = ParameterLocation.Header,
+                        Name = "X\"Api"
+                    },
+                    ["safeHeader"] = new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.ApiKey,
+                        In = ParameterLocation.Header,
+                        Name = "X-Api-Key"
+                    },
+                    ["unsafeQuery"] = new OpenApiSecurityScheme
+                    {
+                        Type = SecuritySchemeType.ApiKey,
+                        In = ParameterLocation.Query,
+                        Name = "X\"Api"
+                    }
+                }
+            },
+            Paths = null!
+        };
+
+        AttributeStringValidator.Validate(document, diagnostic);
+
+        diagnostic.Errors.Should().HaveCount(1);
+        diagnostic.Errors[0].Pointer.Should().Be("unsafeHeader");
+        diagnostic.Errors[0].Message.Should().Contain("Security scheme 'unsafeHeader'");
+    }
+
+    [Test]
+    public void Validate_Should_Report_Path_And_Header_Errors_And_Skip_Null_Branches()
+    {
+        OpenApiDiagnostic diagnostic = new();
+        OpenApiDocument document = new()
+        {
+            Paths = new OpenApiPaths
+            {
+                ["/nullpath"] = null!,
+                ["unsafe\"path"] = new OpenApiPathItem
+                {
+                    Operations = null!
+                },
+                ["/safe"] = new OpenApiPathItem
+                {
+                    Operations = new Dictionary<HttpMethod, OpenApiOperation>
+                    {
+                        [HttpMethod.Get] = new OpenApiOperation
+                        {
+                            Parameters = null!
+                        },
+                        [HttpMethod.Post] = null!,
+                        [HttpMethod.Put] = new OpenApiOperation
+                        {
+                            Parameters = new List<IOpenApiParameter>
+                            {
+                                new OpenApiParameter
+                                {
+                                    In = ParameterLocation.Header,
+                                    Name = "X\"Bad"
+                                },
+                                new OpenApiParameter
+                                {
+                                    In = ParameterLocation.Header,
+                                    Name = "X-Good"
+                                },
+                                new OpenApiParameter
+                                {
+                                    In = ParameterLocation.Query,
+                                    Name = "X\"Ignored"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        AttributeStringValidator.Validate(document, diagnostic);
+
+        diagnostic.Errors.Should().HaveCount(2);
+        diagnostic.Errors.Select(x => x.Pointer).Should().Contain(new[] { "unsafe\"path", "X\"Bad" });
+    }
+}
