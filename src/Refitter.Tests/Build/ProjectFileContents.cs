0bbf2efdbdee393432d304a8db225272079b1964
Christian Helle <christian.helle@outlook.com>
2026-07-27 23:17:43 +0200

    update deps to 10.0.x in net8.0 warnings-as-errors test template for Refit 14.0.1

--- src/Refitter.Tests/Build/ProjectFileContents.cs
+++ src/Refitter.Tests/Build/ProjectFileContents.cs
@@ -42,13 +42,13 @@ public static class ProjectFileContents
   </PropertyGroup>
   <ItemGroup>
     <PackageReference Include=""Refit.HttpClientFactory"" Version=""14.0.1"" />
-    <PackageReference Include=""System.Text.Json"" Version=""8.0.5"" />
-    <PackageReference Include=""System.ComponentModel.Annotations"" Version=""4.5.0"" />
+    <PackageReference Include=""System.Text.Json"" Version=""10.0.0"" />
+    <PackageReference Include=""System.ComponentModel.Annotations"" Version=""5.0.0"" />
     <PackageReference Include=""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
-    <PackageReference Include=""Microsoft.Extensions.DependencyInjection"" Version=""8.0.1"" />
-    <PackageReference Include=""Microsoft.Extensions.Http.Polly"" Version=""8.0.11"" />
-    <PackageReference Include=""Microsoft.Extensions.Http.Resilience"" Version=""8.10.0"" />
-    <PackageReference Include=""Microsoft.Extensions.Options.ConfigurationExtensions"" Version=""8.0.0"" />
+    <PackageReference Include=""Microsoft.Extensions.DependencyInjection"" Version=""10.0.10"" />
+    <PackageReference Include=""Microsoft.Extensions.Http.Polly"" Version=""10.0.10"" />
+    <PackageReference Include=""Microsoft.Extensions.Http.Resilience"" Version=""10.1.0"" />
+    <PackageReference Include=""Microsoft.Extensions.Options.ConfigurationExtensions"" Version=""10.0.10"" />
     <PackageReference Include=""Polly.Contrib.WaitAndRetry"" Version=""1.1.1"" />
     <PackageReference Include=""System.Reactive"" Version=""6.0.1"" />
     <PackageReference Include=""Apizr.Integrations.FileTransfer.MediatR"" Version=""6.4.0"" />
