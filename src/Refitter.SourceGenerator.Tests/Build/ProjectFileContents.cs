d2aee461431670e032c7d0c7ea3e9039718760f8
Christian Helle <christian.helle@outlook.com>
2026-07-27 22:44:21 +0200

    update extension packages to 10.0.10 in source gen test templates

--- src/Refitter.SourceGenerator.Tests/Build/ProjectFileContents.cs
+++ src/Refitter.SourceGenerator.Tests/Build/ProjectFileContents.cs
@@ -16,9 +16,9 @@ public static class ProjectFileContents
     <PackageReference Include=""System.ComponentModel.Annotations"" Version=""4.5.0"" />
     <PackageReference Include=""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
     <PackageReference Include=""Microsoft.Extensions.DependencyInjection"" Version=""10.0.10"" />
-    <PackageReference Include=""Microsoft.Extensions.Http.Polly"" Version=""10.0.9"" />
+    <PackageReference Include=""Microsoft.Extensions.Http.Polly"" Version=""10.0.10"" />
     <PackageReference Include=""Microsoft.Extensions.Http.Resilience"" Version=""10.1.0"" />
-    <PackageReference Include=""Microsoft.Extensions.Options.ConfigurationExtensions"" Version=""10.0.9"" />
+    <PackageReference Include=""Microsoft.Extensions.Options.ConfigurationExtensions"" Version=""10.0.10"" />
     <PackageReference Include=""Polly.Contrib.WaitAndRetry"" Version=""1.1.1"" />
     <PackageReference Include=""System.Reactive"" Version=""6.0.1"" />
     <PackageReference Include=""Apizr.Integrations.FileTransfer.MediatR"" Version=""6.4.0"" />
@@ -45,9 +45,9 @@ public static class ProjectFileContents
     <PackageReference Include=""System.ComponentModel.Annotations"" Version=""4.5.0"" />
     <PackageReference Include=""System.Runtime.Serialization.Primitives"" Version=""4.3.0"" />
     <PackageReference Include=""Microsoft.Extensions.DependencyInjection"" Version=""10.0.10"" />
-    <PackageReference Include=""Microsoft.Extensions.Http.Polly"" Version=""10.0.9"" />
+    <PackageReference Include=""Microsoft.Extensions.Http.Polly"" Version=""10.0.10"" />
     <PackageReference Include=""Microsoft.Extensions.Http.Resilience"" Version=""10.1.0"" />
-    <PackageReference Include=""Microsoft.Extensions.Options.ConfigurationExtensions"" Version=""10.0.9"" />
+    <PackageReference Include=""Microsoft.Extensions.Options.ConfigurationExtensions"" Version=""10.0.10"" />
     <PackageReference Include=""Polly.Contrib.WaitAndRetry"" Version=""1.1.1"" />
     <PackageReference Include=""System.Reactive"" Version=""6.0.1"" />
     <PackageReference Include=""Apizr.Integrations.FileTransfer.MediatR"" Version=""6.4.0"" />
