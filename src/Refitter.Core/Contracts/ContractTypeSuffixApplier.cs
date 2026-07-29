ce7561498a7c7f6206d0a5882f2afd2aa491b307
Christian Helle <christian.helle@outlook.com>
2026-07-29 11:06:12 +0200

    mark private nested class as sealed

--- src/Refitter.Core/Contracts/ContractTypeSuffixApplier.cs
+++ src/Refitter.Core/Contracts/ContractTypeSuffixApplier.cs
@@ -75,7 +75,7 @@ internal static class ContractTypeSuffixApplier
     /// <summary>
     /// Syntax rewriter that renames type declarations and all references to them
     /// </summary>
-    private class TypeSuffixRewriter : CSharpSyntaxRewriter
+    private sealed class TypeSuffixRewriter : CSharpSyntaxRewriter
     {
         private readonly Dictionary<string, string> typeRenameMap;
 
