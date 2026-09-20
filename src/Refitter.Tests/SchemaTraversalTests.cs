fa0d1e2fc222e9d424cfbbc27ce1114e26a0e648
Christian Helle <christian.helle@outlook.com>
2026-09-20 16:40:46 +0200

    cover schema traversal through definitions, tuples and path parameters

---
+++ src/Refitter.Tests/SchemaTraversalTests.cs
@@ -0,0 +1,150 @@
+using AwesomeAssertions;
+using NJsonSchema;
+using NSwag;
+using Refitter.Core;
+
+namespace Refitter.Tests;
+
+public class SchemaTraversalTests
+{
+    [Test]
+    public async Task SchemaWalker_Visits_Schemas_Reached_Through_Definitions()
+    {
+        var document = await OpenApiDocument.FromJsonAsync("""
+            {
+              "openapi": "3.0.1",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {},
+              "components": {
+                "schemas": {
+                  "Root": {
+                    "type": "object",
+                    "definitions": {
+                      "Nested": { "type": "string" }
+                    }
+                  }
+                }
+              }
+            }
+            """);
+
+        var visited = new List<JsonSchema>();
+        SchemaWalker.TraverseDocumentSchemas(document, visited.Add);
+
+        visited.Select(s => s.Type).Should().Contain(JsonObjectType.String);
+    }
+
+    [Test]
+    public async Task SchemaWalker_Visits_Schemas_Reached_Through_Tuple_Items()
+    {
+        var document = await OpenApiDocument.FromJsonAsync("""
+            {
+              "openapi": "3.0.1",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {},
+              "components": {
+                "schemas": {
+                  "Root": {
+                    "type": "array",
+                    "items": [{ "type": "string" }]
+                  }
+                }
+              }
+            }
+            """);
+
+        var visited = new List<JsonSchema>();
+        SchemaWalker.TraverseDocumentSchemas(document, visited.Add);
+
+        visited.Should().HaveCountGreaterThan(1);
+    }
+
+    [Test]
+    public async Task SchemaWalker_Visits_Schemas_Reached_Through_Path_Parameters()
+    {
+        var document = await OpenApiDocument.FromJsonAsync("""
+            {
+              "openapi": "3.0.1",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/pets": {
+                  "parameters": [
+                    {
+                      "name": "filter",
+                      "in": "query",
+                      "schema": { "type": "boolean" }
+                    }
+                  ]
+                }
+              }
+            }
+            """);
+
+        var visited = new List<JsonSchema>();
+        SchemaWalker.TraverseDocumentSchemas(document, visited.Add);
+
+        visited.Select(s => s.Type).Should().Contain(JsonObjectType.Boolean);
+    }
+
+    [Test]
+    public async Task SchemaCleaner_Keeps_Schemas_Reached_Through_Dictionary_Key()
+    {
+        var document = await OpenApiDocument.FromJsonAsync("""
+            {
+              "openapi": "3.0.1",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {
+                "/pets": {
+                  "get": {
+                    "operationId": "GetPets",
+                    "responses": {
+                      "200": {
+                        "description": "ok",
+                        "content": {
+                          "application/json": {
+                            "schema": { "$ref": "#/components/schemas/Root" }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              },
+              "components": {
+                "schemas": {
+                  "Root": {
+                    "type": "object",
+                    "additionalProperties": { "$ref": "#/components/schemas/Value" }
+                  },
+                  "Value": { "type": "string" },
+                  "Unused": { "type": "integer" }
+                }
+              }
+            }
+            """);
+
+        new SchemaCleaner(document, []).RemoveUnreferencedSchema();
+
+        document.Components.Schemas.Should().ContainKey("Root");
+        document.Components.Schemas.Should().ContainKey("Value");
+        document.Components.Schemas.Should().NotContainKey("Unused");
+    }
+
+    [Test]
+    public async Task SchemaCleaner_Handles_Null_Schema_Argument()
+    {
+        var document = await OpenApiDocument.FromJsonAsync("""
+            {
+              "openapi": "3.0.1",
+              "info": { "title": "Test", "version": "1.0" },
+              "paths": {}
+            }
+            """);
+
+        var cleaner = new SchemaCleaner(document, []);
+
+        var act = () => cleaner.RemoveUnreferencedSchema();
+
+        act.Should().NotThrow();
+    }
+}
