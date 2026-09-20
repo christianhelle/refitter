7e54c473ebae29e67978e36affb60a623618bff1
Christian Helle <christian.helle@outlook.com>
2026-09-20 16:35:58 +0200

    cover json escape decoding branches

---
+++ src/Refitter.Tests/Regression/JsonEscapeDecodingBranchTests.cs
@@ -0,0 +1,61 @@
+using AwesomeAssertions;
+using Refitter.Core;
+
+namespace Refitter.Tests.Regression;
+
+public class JsonEscapeDecodingBranchTests
+{
+    [Test]
+    [Arguments(null)]
+    [Arguments("")]
+    public void DecodeJsonEscapedText_Returns_Input_For_Null_Or_Empty(string? input)
+    {
+        var result = XmlDocumentationGenerator.DecodeJsonEscapedText(input!);
+
+        result.Should().Be(input);
+    }
+
+    [Test]
+    public void DecodeJsonEscapedText_Decodes_Backslash_Escape()
+    {
+        var result = XmlDocumentationGenerator.DecodeJsonEscapedText(@"a\\b");
+
+        result.Should().Be(@"a\b");
+    }
+
+    [Test]
+    public void DecodeJsonEscapedText_Decodes_Forward_Slash_Escape()
+    {
+        var result = XmlDocumentationGenerator.DecodeJsonEscapedText(@"a\/b");
+
+        result.Should().Be("a/b");
+    }
+
+    [Test]
+    public void DecodeJsonEscapedText_Decodes_Backspace_Escape()
+    {
+        var result = XmlDocumentationGenerator.DecodeJsonEscapedText(@"a\bb");
+
+        result.Should().Contain("a");
+        result.Should().Contain("b");
+    }
+
+    [Test]
+    public void DecodeJsonEscapedText_Decodes_Form_Feed_Escape()
+    {
+        var result = XmlDocumentationGenerator.DecodeJsonEscapedText(@"a\fb");
+
+        result.Should().Contain("a");
+        result.Should().Contain("b");
+        result.Should().NotContain("\f");
+    }
+
+    [Test]
+    public void DecodeJsonEscapedText_Preserves_Unknown_Escape_Sequence()
+    {
+        var result = XmlDocumentationGenerator.DecodeJsonEscapedText(@"a\qb");
+
+        result.Should().Contain("a");
+        result.Should().Contain("b");
+    }
+}
