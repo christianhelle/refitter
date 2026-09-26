c31e3d5721e71058c065b7826ea24c816beffedf
Christian Helle <christian.helle@outlook.com>
2026-09-26 16:20:14 +0200

    Register generated interface names so collisions get numbered

--- src/Refitter.Core/Generation/InterfaceGenerator.cs
+++ src/Refitter.Core/Generation/InterfaceGenerator.cs
@@ -114,6 +114,7 @@ internal class InterfaceGenerator
         var rawInterfaceName = partitioning.GetInterfaceName(string.Empty, title, baseOperationName);
         var interfaceNameSuffix = partitioning.GetInterfaceNameSuffix();
         var interfaceName = IdentifierUtils.Counted(knownInterfaceIdentifiers, rawInterfaceName, interfaceNameSuffix);
+        knownInterfaceIdentifiers.Add(interfaceName);
 
         var code = new StringBuilder();
         var dynamicQuerystringParametersCodeBuilder = new StringBuilder();
@@ -165,6 +166,7 @@ internal class InterfaceGenerator
         var rawInterfaceName = partitioning.GetInterfaceName(groupKey, title, baseOperationName);
         var interfaceNameSuffix = partitioning.GetInterfaceNameSuffix();
         var interfaceName = IdentifierUtils.Counted(knownInterfaceIdentifiers, rawInterfaceName, interfaceNameSuffix);
+        knownInterfaceIdentifiers.Add(interfaceName);
 
         var code = new StringBuilder();
         partitioning.AppendInterfaceDocumentation(document, docGenerator, groupKey, representativeOperation, code);
